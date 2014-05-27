472f9cdbb47e562fc3887a54a3235932354bb2c1
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-05-27 10:54:25 +0200

    Frame draft

---
+++ aslam_frames/include/aslam/frames/VisualFrame.h
@@ -0,0 +1,51 @@
+#ifndef ASLAM_FRAMES_VISUAL_FRAME_H_
+#define ASLAM_FRAMES_VISUAL_FRAME_H_
+
+#include <memory>
+#include <aslam/common/macros.h>
+#include <Eigen/Dense>
+
+namespace aslam {
+namespace cameras {
+class Camera;
+}
+
+class VisualFrame {
+ public:
+  typedef Eigen::Matrix<char, Eigen::Dynamic, Eigen::Dynamic> DescriptorsT;
+  ASLAM_POINTER_TYPEDEFS(VisualFrame);
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  virtual bool operator==(const VisualFrame& other) const;
+  /// \brief The keypoints stored in a frame.
+  const Eigen::Matrix2Xd& getKeypoints() const;
+  /// \brief The descriptors stored in a frame.
+  const DescriptorsT& getDescriptors() const;
+
+  /// \brief A pointer to the keypoints, can be used to swap in new data.
+  Eigen::Matrix2Xd* getKeypointsMutable();
+  /// \brief A pointer to the descriptors, can be used to swap in new data.
+  DescriptorsT* getDescriptorsMutable();
+
+  /// \brief Return pointer location of the descriptor pointed to by index.
+  const char* getDescriptor(size_t index) const;
+  /// \brief Return block expression of the keypoint pointed to by index.
+  const Eigen::Block<Eigen::Matrix2Xd, 2, 1> getKeypoint(size_t index) const;
+
+  /// \brief Replace (copy) the internal keypoints by the passed ones.
+  const void setKeypoints(const Eigen::Matrix2Xd& keypoints);
+  /// \brief Replace (copy) the internal descriptors by the passed ones.
+  const void setDescriptors(const DescriptorsT& descriptors);
+
+  /// \brief The camera geometry.
+  const std::shared_ptr<const cameras::Camera> getCameraGeometry() const;
+  /// \brief Set the camera geometry.
+  void setCameraGeometry(const std::shared_ptr<cameras::Camera>& camera);
+
+ private:
+  Eigen::Matrix2Xd keypoints_;
+  DescriptorsT descriptors_;
+  std::shared_ptr<cameras::Camera> camera_geometry_;
+};
+}  // namespace aslam
+#endif  // ASLAM_FRAMES_VISUAL_FRAME_H_
---
+++ aslam_frames/src/VisualFrame.cc
@@ -0,0 +1,51 @@
+#include <aslam/frames/VisualFrame.h>
+
+namespace aslam {
+virtual bool VisualFrame::operator==(const VisualFrame& other) const {
+  bool same = true;
+  same &= keypoints_ == other.keypoints_;
+  same &= descriptors_ == other.descriptors_;
+  same &= static_cast<bool>(camera_geometry_) ==
+      static_cast<bool>(other.camera_geometry_);
+  if (same) {
+    same &= *camera_geometry_ == *other.camera_geometry_;
+  }
+  return same;
+}
+const Eigen::Matrix2Xd& VisualFrame::getKeypoints() const {
+  return keypoints_;
+}
+const VisualFrame::DescriptorsT& VisualFrame::getDescriptors() const {
+  return descriptors_;
+}
+Eigen::Matrix2Xd* VisualFrame::getKeypointsMutable() {
+  return &keypoints_;
+}
+DescriptorsT* VisualFrame::getDescriptorsMutable() {
+  return &descriptors_;
+}
+
+const char* VisualFrame::getDescriptor(size_t index) const {
+  CHECK_LT(index, descriptors_.cols());
+  return &descriptors_.coeffRef(0, index);
+}
+const Eigen::Block<Eigen::Matrix2Xd, 2, 1> VisualFrame::getKeypoint(size_t index) const {
+  CHECK_LT(index, keypoints_.cols());
+  return keypoints_.block<2, 1>(0, index);
+}
+
+const void VisualFrame::setKeypoints(const Eigen::Matrix2Xd& keypoints) {
+  keypoints_ = keypoints;
+}
+const void VisualFrame::setDescriptors(const DescriptorsT& descriptors) {
+  descriptors_ = descriptors;
+}
+
+const std::shared_ptr<const cameras::Camera> VisualFrame::getCameraGeometry() const {
+  return camera_geometry_;
+}
+void VisualFrame::setCameraGeometry(const std::shared_ptr<cameras::Camera>& camera) {
+  camera_geometry_ = camera;
+}
+
+}  // namespace aslam
