8c65f51e1c7927aa9fadffde27b6e895aaf4d0b6
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-05-29 20:18:32 +0200

    Add copy of pauls camera so we can look at the diff after refactoring

--- aslam_cameras/test/test_pinhole-camera.cc
+++ aslam_cameras/test/test_pinhole-camera.cc
@@ -1,9 +1,6 @@
 #include <aslam/common/entrypoint.h>
-#include <aslam/cameras/pinhole-camera.h>
 
 TEST(PinHoleCamera, SetGetLineDelay) {
-  aslam::PinholeCamera camera;
-
 }
 
 ASLAM_UNITTEST_ENTRYPOINT
