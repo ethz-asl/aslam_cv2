d98808ea26dcf07f5b63201e1a1fb2406ee5106c
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-05-29 20:19:58 +0200

    Refactor of pinhole projection into pinhole camera

--- aslam_cameras/test/test_pinhole-camera.cc
+++ aslam_cameras/test/test_pinhole-camera.cc
@@ -1,6 +1,9 @@
 #include <aslam/common/entrypoint.h>
+#include <aslam/cameras/pinhole-camera.h>
 
 TEST(PinHoleCamera, SetGetLineDelay) {
+  aslam::PinholeCamera camera;
+
 }
 
 ASLAM_UNITTEST_ENTRYPOINT
