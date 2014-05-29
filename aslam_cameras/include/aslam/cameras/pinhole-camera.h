d0db8f81da14a698d9af1b2ea30d92439938a637
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-05-29 20:29:04 +0200

    move code to reduce diff

--- aslam_cameras/include/aslam/cameras/pinhole-camera.h
+++ aslam_cameras/include/aslam/cameras/pinhole-camera.h
@@ -9,6 +9,7 @@ class PinholeCamera : public Camera {
  public:
   ASLAM_POINTER_TYPEDEFS(PinholeCamera);
   ASLAM_DISALLOW_EVIL_CONSTRUCTORS(PinholeCamera);
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
 
   enum {
     IntrinsicsDimension = 4
@@ -28,6 +29,63 @@ class PinholeCamera : public Camera {
   PinholeCamera(const sm::PropertyTree& config);
 
   virtual ~PinholeCamera();
+
+
+  /// Project a point expressed in euclidean coordinates to a 2d image measurement.
+  virtual bool euclideanToKeypoint(const Eigen::Vector3d& point,
+                                   Eigen::Matrix<double, 2, 1>* out_point) const;
+  /// Project a point expressed in euclidean coordinates to a 2d image measurement
+  /// and calculate the relevant jacobian.
+  virtual bool euclideanToKeypoint(const Eigen::Vector3d & point,
+                                   Eigen::Matrix<double, 2, 1>* out_point,
+                                   Eigen::Matrix<double, 2, 3>* out_jacobian) const;
+
+  /// Project a point expressed in homogenous coordinates to a 2d image measurement.
+  virtual bool homogeneousToKeypoint(const Eigen::Vector4d& homogeneous_point,
+                                     Eigen::Matrix<double, 2, 1>* out_point) const;
+
+  /// Project a point expressed in homogenous coordinates to a 2d image measurement
+  /// and calculate the relevant jacobian.
+  virtual bool homogeneousToKeypoint(const Eigen::Vector4d & homogeneous_point,
+                                     Eigen::Matrix<double, 2, 1>* out_point,
+                                     Eigen::Matrix<double, 2, 4>* out_jacobian) const;
+
+  /// Compute the 3d bearing vector in euclidean coordinates from the 2d image measurement.
+  virtual bool keypointToEuclidean(const Eigen::Matrix<double, 2, 1>& keypoint,
+                                   Eigen::Matrix<double, 3, 1>* out_point) const;
+
+
+  /// Compute the 3d bearing vector in euclidean coordinates and the relevant jacobian
+  /// from the 2d image measurement.
+  virtual bool keypointToEuclidean(const Eigen::Vector2d& keypoint,
+                                   Eigen::Matrix<double, 3, 1>* out_point,
+                                   Eigen::Matrix<double, 3, 2>* out_jacobian) const;
+
+  /// Compute the 3d bearing vector in homogenous coordinates from the 2d image measurement.
+  virtual bool keypointToHomogeneous(Eigen::Vector2d const& keypoint,
+                                     Eigen::Matrix<double, 4, 1>* out_point) const;
+
+  /// Compute the 3d bearing vector in homogeneous coordinates and the relevant
+  /// jacobian from the 2d image measurement.
+  virtual bool keypointToHomogeneous(Eigen::Vector2d const& keypoint,
+                                     Eigen::Matrix<double, 4, 1>* out_point,
+                                     Eigen::Matrix<double, 4, 2>* out_jacobian) const;
+
+  virtual bool euclideanToKeypointIntrinsicsJacobian(
+      const Eigen::Matrix<double, 3, 1>& p,
+      Eigen::Matrix<double, 2, Eigen::Dynamic>* outJi) const;
+
+  virtual bool euclideanToKeypointDistortionJacobian(
+      const Eigen::Matrix<double, 3, 1>& p,
+      Eigen::Matrix<double, 2, Eigen::Dynamic>* outJd) const;
+
+  virtual bool homogeneousToKeypointIntrinsicsJacobian(
+      const Eigen::Matrix<double, 4, 1>& p,
+      Eigen::Matrix<double, 2, Eigen::Dynamic>* outJi) const;
+
+  virtual bool homogeneousToKeypointDistortionJacobian(
+      const Eigen::Matrix<double, 4, 1>& p,
+      Eigen::Matrix<double, 2, Eigen::Dynamic>* outJd) const;
 
   virtual bool operator==(const PinholeCamera& other) const;
 
@@ -97,62 +155,6 @@ class PinholeCamera : public Camera {
     return _rv;
   }
 
-  /// Project a point expressed in euclidean coordinates to a 2d image measurement.
-  virtual bool euclideanToKeypoint(const Eigen::Vector3d& point,
-                                   Eigen::Matrix<double, 2, 1>* out_point) const;
-  /// Project a point expressed in euclidean coordinates to a 2d image measurement
-  /// and calculate the relevant jacobian.
-  virtual bool euclideanToKeypoint(const Eigen::Vector3d & point,
-                                   Eigen::Matrix<double, 2, 1>* out_point,
-                                   Eigen::Matrix<double, 2, 3>* out_jacobian) const;
-
-  /// Project a point expressed in homogenous coordinates to a 2d image measurement.
-  virtual bool homogeneousToKeypoint(const Eigen::Vector4d& homogeneous_point,
-                                     Eigen::Matrix<double, 2, 1>* out_point) const;
-
-  /// Project a point expressed in homogenous coordinates to a 2d image measurement
-  /// and calculate the relevant jacobian.
-  virtual bool homogeneousToKeypoint(const Eigen::Vector4d & homogeneous_point,
-                                     Eigen::Matrix<double, 2, 1>* out_point,
-                                     Eigen::Matrix<double, 2, 4>* out_jacobian) const;
-
-  /// Compute the 3d bearing vector in euclidean coordinates from the 2d image measurement.
-  virtual bool keypointToEuclidean(const Eigen::Matrix<double, 2, 1>& keypoint,
-                                   Eigen::Matrix<double, 3, 1>* out_point) const;
-
-
-  /// Compute the 3d bearing vector in euclidean coordinates and the relevant jacobian
-  /// from the 2d image measurement.
-  virtual bool keypointToEuclidean(const Eigen::Vector2d& keypoint,
-                                   Eigen::Matrix<double, 3, 1>* out_point,
-                                   Eigen::Matrix<double, 3, 2>* out_jacobian) const;
-
-  /// Compute the 3d bearing vector in homogenous coordinates from the 2d image measurement.
-  virtual bool keypointToHomogeneous(Eigen::Vector2d const& keypoint,
-                                     Eigen::Matrix<double, 4, 1>* out_point) const;
-
-  /// Compute the 3d bearing vector in homogeneous coordinates and the relevant
-  /// jacobian from the 2d image measurement.
-  virtual bool keypointToHomogeneous(Eigen::Vector2d const& keypoint,
-                                     Eigen::Matrix<double, 4, 1>* out_point,
-                                     Eigen::Matrix<double, 4, 2>* out_jacobian) const;
-
-  virtual bool euclideanToKeypointIntrinsicsJacobian(
-      const Eigen::Matrix<double, 3, 1>& p,
-      Eigen::Matrix<double, 2, Eigen::Dynamic>* outJi) const;
-
-  virtual bool euclideanToKeypointDistortionJacobian(
-      const Eigen::Matrix<double, 3, 1>& p,
-      Eigen::Matrix<double, 2, Eigen::Dynamic>* outJd) const;
-
-  virtual bool homogeneousToKeypointIntrinsicsJacobian(
-      const Eigen::Matrix<double, 4, 1>& p,
-      Eigen::Matrix<double, 2, Eigen::Dynamic>* outJi) const;
-
-  virtual bool homogeneousToKeypointDistortionJacobian(
-      const Eigen::Matrix<double, 4, 1>& p,
-      Eigen::Matrix<double, 2, Eigen::Dynamic>* outJd) const;
-
   // \brief creates a random valid keypoint.
   virtual Eigen::Matrix<double, 2, 1> createRandomKeypoint() const;
 
@@ -185,23 +187,25 @@ class PinholeCamera : public Camera {
   virtual void getBorderRays(Eigen::MatrixXd & rays);
 
   // TODO(slynen): Reintegrate once we have the calibration targets.
-//  /// \brief initialize the intrinsics based on a list of views of a gridded calibration target
-//  /// \return true on success
-//  virtual bool initializeIntrinsics(const std::vector<GridCalibrationTargetObservation> &observations);
-//
-//  /// \brief compute the reprojection error based on a checkerboard observation.
-//  /// \return the number of corners successfully observed and projected
-//  virtual size_t computeReprojectionError(
-//      const GridCalibrationTargetObservation & obs,
-//      const sm::kinematics::Transformation & T_target_camera,
-//      double & outErr) const;
-//
-//  /// \brief estimate the transformation of the camera with respect to the calibration target
-//  ///        On success out_T_t_c is filled in with the transformation that takes points from
-//  ///        the camera frame to the target frame
-//  /// \return true on success
-//  virtual bool estimateTransformation(const GridCalibrationTargetObservation & obs,
-//                                      sm::kinematics::Transformation & out_T_t_c) const;
+/*
+  /// \brief initialize the intrinsics based on a list of views of a gridded calibration target
+  /// \return true on success
+  virtual bool initializeIntrinsics(const std::vector<GridCalibrationTargetObservation> &observations);
+
+  /// \brief compute the reprojection error based on a checkerboard observation.
+  /// \return the number of corners successfully observed and projected
+  virtual size_t computeReprojectionError(
+      const GridCalibrationTargetObservation & obs,
+      const sm::kinematics::Transformation & T_target_camera,
+      double & outErr) const;
+
+  /// \brief estimate the transformation of the camera with respect to the calibration target
+  ///        On success out_T_t_c is filled in with the transformation that takes points from
+  ///        the camera frame to the target frame
+  /// \return true on success
+  virtual bool estimateTransformation(const GridCalibrationTargetObservation & obs,
+                                      sm::kinematics::Transformation & out_T_t_c) const;
+*/
 
  private:
   void updateTemporaries();
