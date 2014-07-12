c3143623325fffbc3ddfc595ca2b5164cd4f2730
Paul Furgale <paul.furgale@gmail.com>
2014-07-12 20:28:55 +0200

    Draft of a camera rig

---
+++ aslam_cv_common/include/aslam/common/types.h
@@ -0,0 +1,9 @@
+#include <kindr/minimal/QuatTransformation.h>
+
+namespace aslam {
+
+typedef kindr::minimal::QuatTransformation Transformation;
+typedef std::shared_ptr<Transformation> TransformationPtr;
+typedef kindr::minimal::QuatTransformation::Rotation Rotation;
+
+} // namespace aslam
