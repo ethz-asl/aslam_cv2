98333f967470a04f7566184d663115cd8c0984e2
Paul Furgale <paul.furgale@gmail.com>
2014-08-23 08:41:43 +0200

    Header prototyping

--- aslam_cv/include/aslam/cameras/camera.h
+++ aslam_cv/include/aslam/cameras/camera.h
@@ -24,120 +24,9 @@ class Camera {
   //explicit Camera(const sm::PropertyTree& property_tree);
   virtual ~Camera();
 
-  virtual bool operator==(const Camera& other) const;
-
-  virtual uint32_t imageWidth() const = 0;
-  virtual uint32_t imageHeight() const = 0;
-
-  /// Project a point expressed in euclidean coordinates to a 2d image measurement.
-  virtual bool euclideanToKeypoint(const Eigen::Vector3d& point,
-                                   Eigen::Matrix<double, 2, 1>* out_point) const = 0;
-  /// Project a point expressed in euclidean coordinates to a 2d image measurement
-  /// and calculate the relevant jacobian.
-  virtual bool euclideanToKeypoint(const Eigen::Vector3d & point,
-                                   Eigen::Matrix<double, 2, 1>* out_point,
-                                   Eigen::Matrix<double, 2, 3>* out_jacobian) const = 0;
-
-  /// Project a point expressed in homogenous coordinates to a 2d image measurement.
-  virtual bool homogeneousToKeypoint(const Eigen::Vector4d& homogeneous_point,
-                                     Eigen::Matrix<double, 2, 1>* out_point) const = 0;
-
-  /// Project a point expressed in homogenous coordinates to a 2d image measurement
-  /// and calculate the relevant jacobian.
-  virtual bool homogeneousToKeypoint(const Eigen::Vector4d & homogeneous_point,
-                                     Eigen::Matrix<double, 2, 1>* out_point,
-                                     Eigen::Matrix<double, 2, 4>* out_jacobian) const = 0;
-
-  /// Compute the 3d bearing vector in euclidean coordinates from the 2d image measurement.
-  virtual bool keypointToEuclidean(const Eigen::Matrix<double, 2, 1>& keypoint,
-                                   Eigen::Matrix<double, 3, 1>* out_point) const = 0;
-
-
-  /// Compute the 3d bearing vector in euclidean coordinates and the relevant jacobian
-  /// from the 2d image measurement.
-  virtual bool keypointToEuclidean(const Eigen::Vector2d& keypoint,
-                                   Eigen::Matrix<double, 3, 1>* out_point,
-                                   Eigen::Matrix<double, 3, 2>* out_jacobian) const = 0;
-
-  /// Compute the 3d bearing vector in homogenous coordinates from the 2d image measurement.
-  virtual bool keypointToHomogeneous(Eigen::Vector2d const& keypoint,
-                                     Eigen::Matrix<double, 4, 1>* out_point) const = 0;
-
-  /// Compute the 3d bearing vector in homogeneous coordinates and the relevant
-  /// jacobian from the 2d image measurement.
-  virtual bool keypointToHomogeneous(Eigen::Vector2d const& keypoint,
-                                     Eigen::Matrix<double, 4, 1>* out_point,
-                                     Eigen::Matrix<double, 4, 2>* out_jacobian) const = 0;
-
-  /// Compute the jacobian of the image measurement w.r.t. the intrinsics.
-  virtual bool euclideanToKeypointIntrinsicsJacobian(
-      const Eigen::Matrix<double, 3, 1>& point,
-      Eigen::Matrix<double, 2, Eigen::Dynamic>* out_jacobian) const = 0;
-
-  /// Compute the jacobian of the image measurement w.r.t. the distortion.
-  virtual bool euclideanToKeypointDistortionJacobian(
-      const Eigen::Matrix<double, 3, 1>& point,
-      Eigen::Matrix<double, 2, Eigen::Dynamic>* out_jacobian) const = 0;
-
-  /// Compute the jacobian of the image measurement w.r.t. the intrinsics.
-  virtual bool homogeneousToKeypointIntrinsicsJacobian(
-      const Eigen::Matrix<double, 4, 1>& homogeneous_point,
-      Eigen::Matrix<double, 2, Eigen::Dynamic>* out_jacobian) const = 0;
-
-  /// Compute the jacobian of the image measurement w.r.t. the distortion.
-  virtual bool homogeneousToKeypointDistortionJacobian(
-      const Eigen::Matrix<double, 4, 1>& homogeneous_point,
-      Eigen::Matrix<double, 2, Eigen::Dynamic>* out_jacobian) const = 0;
-
-  // \brief creates a random valid keypoint.
-  virtual Eigen::Matrix<double, 2, 1> createRandomKeypoint() const = 0;
-
-  // \brief creates a random visible point. Negative depth means random between
-  /// 0 and 100 meters.
-  virtual Eigen::Matrix<double, 3, 1> createRandomVisiblePoint(double depth) const = 0;
-
-  virtual Eigen::VectorXd& getParametersMutable() = 0;
-
-  virtual double* getParameterMutablePtr() = 0;
-
-  virtual Eigen::Vector2i parameterSize() const = 0;
-
   //////////////////////////////////////////////////////////////
-  // SHUTTER SUPPORT
-  //////////////////////////////////////////////////////////////
-
-  /// Return the temporal offset of a rolling shutter camera.
-  uint64_t getTemporalOffsetNanoSeconds() const {
-    return line_delay_nano_seconds_;
-  }
-
-  /// Set the temporal offset of a rolling shutter camera.
-  void setTemporalOffsetNanoSeconds(uint64_t line_delay_nano_seconds) {
-    line_delay_nano_seconds_ = line_delay_nano_seconds;
-  }
-
-  // The amount of time elapsed between the start of the image and the
-  // keypoint. For a global shutter camera, this can return Duration(0).
-  inline int64_t temporalOffsetNanoSeconds(
-      const Eigen::Matrix<double, 2, 1>& keypoint) const {
-    CHECK_GE(0, keypoint(1));
-    CHECK_LT(imageWidth(), keypoint(1));
-    return static_cast<int64_t>(keypoint(1)) * line_delay_nano_seconds_;
-  }
-
-  //////////////////////////////////////////////////////////////
-  // VALIDITY TESTING
-  //////////////////////////////////////////////////////////////
-
-  virtual bool isValid(const Eigen::Matrix<double, 2, 1>& keypoint) const = 0;
-
-  virtual bool isEuclideanVisible(
-      const Eigen::Matrix<double, 3, 1>& point) const = 0;
-
-  virtual bool isHomogeneousVisible(
-      const Eigen::Matrix<double, 4, 1>& homogeneous_point) const = 0;
-
-  virtual bool isProjectionInvertible() const = 0;
+  /// \name Information about the camera
+  /// @{
 
   /// \brief get the camera id.
   aslam::CameraId getId() const { return id_; }
@@ -151,8 +40,197 @@ class Camera {
   /// \brief set a label for the camera
   void setLabel(const std::string& label) {label_ = label;}
 
+  /// \brief The width of the image
+  virtual uint32_t imageWidth() const = 0;
+
+  /// \brief The height of the image
+  virtual uint32_t imageHeight() const = 0;
+
+  /// \brief Print the internal parameters of the camera in a human-readable form
+  /// Print to the ostream that is passed in. The text is extra
+  /// text used by the calling function. 
+  void print(std::ostream& out = std::cout, const std::string& text = std::string());
+
+  /// @}
+
+  //////////////////////////////////////////////////////////////
+  /// \name Methods to project and back-project points
+  /// @{
+
+  /// Project a point expressed in euclidean coordinates to a 2d image measurement.
+  virtual bool project3(const Eigen::Vector3d& point,
+                                   Eigen::Eigen::Vector2d* out_keypoint) const = 0;
+
+  /// Project a point expressed in euclidean coordinates to a 2d image measurement
+  /// and calculate the relevant jacobian.
+  virtual bool project3(const Eigen::Vector3d & point,
+                                   Eigen::Eigen::Vector2d* out_keypoint,
+                                   Eigen::Matrix<double, 2, 3>* out_jacobian) const = 0;
+
+  /// Project a point expressed in homogenous coordinates to a 2d image measurement.
+  virtual bool project4(const Eigen::Vector4d& point,
+                        Eigen::Eigen::Vector2d* out_keypoint) const = 0;
+
+  /// Project a point expressed in homogenous coordinates to a 2d image measurement
+  /// and calculate the relevant jacobian.
+  virtual bool project4(const Eigen::Vector4d & point,
+                        Eigen::Eigen::Vector2d* out_keypoint,
+                        Eigen::Matrix<double, 2, 4>* out_jacobian) const = 0;
+
+  /// Compute the 3d bearing vector in euclidean coordinates from the 2d image measurement.
+  virtual bool backProject3(const Eigen::Eigen::Vector2d& keypoint,
+                            Eigen::Vector3d* out_keypoint) const = 0;
+
+
+  /// Compute the 3d bearing vector in euclidean coordinates and the relevant jacobian
+  /// from the 2d image measurement.
+  virtual bool backProject3(const Eigen::Vector2d& keypoint,
+                            Eigen::Vector3d* out_point,
+                            Eigen::Matrix<double, 3, 2>* out_jacobian) const = 0;
+
+  /// Compute the 3d bearing vector in homogenous coordinates from the 2d image measurement.
+  virtual bool backProject4(Eigen::Vector2d const& keypoint,
+                            Eigen::Vector4d* out_point) const = 0;
+
+  /// Compute the 3d bearing vector in homogeneous coordinates and the relevant
+  /// jacobian from the 2d image measurement.
+  virtual bool backProject4(Eigen::Vector2d const& keypoint,
+                            Eigen::Vector4d* out_point,
+                            Eigen::Matrix<double, 4, 2>* out_jacobian) const = 0;
+
+  /// Compute the jacobian of the image measurement w.r.t. the intrinsics.
+  virtual bool project3IntrinsicsJacobian(
+      const Eigen::Vector3d& point,
+      Eigen::Matrix<double, 2, Eigen::Dynamic>* out_jacobian) const = 0;
+  
+  /// Compute the jacobian of the image measurement w.r.t. the distortion.
+  virtual bool project3DistortionJacobian(
+      const Eigen::Vector3d& point,
+      Eigen::Matrix<double, 2, Eigen::Dynamic>* out_jacobian) const = 0;
+  
+  /// Compute the jacobian of the image measurement w.r.t. the intrinsics.
+  virtual bool project3IntrinsicsJacobian(
+      const Eigen::Vector4d& point,
+      Eigen::Matrix<double, 2, Eigen::Dynamic>* out_jacobian) const = 0;
+
+  /// Compute the jacobian of the image measurement w.r.t. the distortion.
+  virtual bool project3DistortionJacobian(
+      const Eigen::Vector4d& point,
+      Eigen::Matrix<double, 2, Eigen::Dynamic>* out_jacobian) const = 0;
+
+  /// @}
+
+  //////////////////////////////////////////////////////////////
+  /// \name Functional methods to project and back-project points
+  /// @{
+  
+  /// This function projects a point into the image using the
+  /// intrinsics parameters and distortion parameters that are
+  /// passed in as arguments. If any of the Jacobians are nonnull,
+  /// they should be filled in with the Jacobian with respect to
+  /// small changes in the argument.
+  virtual bool project3Functional(const Eigen::VectorXd& intrinsics_params,
+                                  const Eigen::VectorXd& distortion_params,
+                                  const Eigen::Vector3d& point,
+                                  Eigen::Vector2d * out_point,
+                                  Eigen::Matrix<double, 2, Eigen::Dynamic>* out_intrinsics_jacobian,
+                                  Eigen::Matrix<double, 2, Eigen::Dynamic>* out_distortion_jacobian,
+                                  Eigen::Matrix<double, 2, Eigen::Dynamic>* out_point_jacobian) const = 0;
+                         
+  /// @}
+  
+  //////////////////////////////////////////////////////////////
+  /// \name Methods to support rolling shutter cameras
+  /// @{
+
+  /// Return the temporal offset of a rolling shutter camera.
+  uint64_t getLineDelayNanoSeconds() const {
+    return line_delay_nano_seconds_;
+  }
+
+  /// Set the temporal offset of a rolling shutter camera.
+  void setLineDelayNanoSeconds(uint64_t line_delay_nano_seconds) {
+    line_delay_nano_seconds_ = line_delay_nano_seconds;
+  }
+
+  // The amount of time elapsed between the first row of the image and the
+  // keypoint. For a global shutter camera, this can return Duration(0).
+  inline int64_t temporalOffsetNanoSeconds(
+      const Eigen::Eigen::Vector2d& keypoint) const {
+    // Don't check validity. This allows points to wander in and out
+    // of the frame during optimization
+    return static_cast<int64_t>(keypoint(1)) * line_delay_nano_seconds_;
+  }
+
+  // The amount of time elapsed between the first row of the image and the
+  // last row of the image. For a global shutter camera, this can return 0.
+  inline int64_t maxTemporalOffsetNanoSeconds() const {
+    return this->imageHeight() * line_delay_nano_seconds_
+  }
+
+
+  /// @}
+
+  //////////////////////////////////////////////////////////////
+  /// \name Methods to test validity and visibility
+  /// @{
+
+  /// Is the image point within the image bounds?
+  virtual bool isVisible(const Eigen::Eigen::Vector2d& keypoint) const = 0;
+
+  /// Can the projection function be run on this point?
+  /// This doesn't test if the projected point is visible, only
+  /// if the projection function can be run without numerical
+  /// errors or singularities..
+  virtual bool isProjectable3(
+      const Eigen::Vector3d& point) const = 0;
+
+  /// Can the projection function be run on this point?
+  /// This doesn't test if the projected point is visible, only
+  /// if the projection function can be run without numerical
+  /// errors or singularities.
+  virtual bool isProjectable4(
+      const Eigen::Vector4d& point) const = 0;
+
+  /// @}
+
+
+  //////////////////////////////////////////////////////////////
+  /// \name Methods to support unit testing.
+  /// @{
+
+  /// \brief creates a random valid keypoint.
+  virtual Eigen::Eigen::Vector2d createRandomKeypoint() const = 0;
+  
+  /// \brief creates a random visible point. Negative depth means random between
+  /// 0 and 100 meters.
+  virtual Eigen::Vector3d createRandomVisiblePoint(double depth) const = 0;
+
+  /// \brief is this camera equal to another
+  virtual bool isEqual(const Camera& other, double tolerance=1e-9) const;
+  
+  /// @}
+
+  /// \name Methods to support optimizaiton
+  /// @{
+
+  /// Get the intrinsic parameters. 
+  /// This should include distortion parameters
+  virtual Eigen::VectorXd& getParameters() const = 0;
+
+  /// Get the intrinsic parameters (mutable).
+  virtual Eigen::VectorXd& getParametersMutable() = 0;
+
+  /// Get the intrinsic parameters (memory pointer).
+  virtual double* getParameterMutablePtr() = 0;
+
+  /// How many intrinsic parameters are there?
+  virtual size_t getParameterSize() const = 0;
+
+  /// @}
+
  private:
-  /// The delay per scan line for a rolling shutter camera.
+  /// The delay per scanline for a rolling shutter camera.
   uint64_t line_delay_nano_seconds_;
   /// A label for this camera, a name.
   std::string label_;
