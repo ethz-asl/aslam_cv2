aa31f58afca3d2391d244c34de45495a4a89446e
Paul Furgale <paul.furgale@gmail.com>
2014-08-24 16:58:28 +0200

    Changed the names for camera rig and multi frame

--- aslam_cv/src/ncameras.cc
+++ aslam_cv/src/ncameras.cc
@@ -1,4 +1,4 @@
-#include <aslam/cameras/camera-rig.h>
+#include <aslam/cameras/ncameras.h>
 #include <aslam/common/predicates.h>
 #include <glog/logging.h>
 #include <sm/PropertyTree.hpp>
@@ -6,9 +6,9 @@
 namespace aslam {
 
 /// \brief default constructor builds an empty camera rig
-CameraRig::CameraRig() { }
+NCameras::NCameras() { }
 
-CameraRig::CameraRig(const CameraRigId& id,
+NCameras::NCameras(const NCamerasId& id,
                      const TransformationVector& T_C_B, 
                      const std::vector<Camera::Ptr>& cameras,
                      const std::string& label) :
@@ -18,13 +18,13 @@ CameraRig::CameraRig(const CameraRigId& id,
 }
 
 /// \brief initialize from a property tree
-CameraRig::CameraRig(const sm::PropertyTree& /* propertyTree */) {
+NCameras::NCameras(const sm::PropertyTree& /* propertyTree */) {
   // \todo(PTF) fill in
 }
   
-CameraRig::~CameraRig() {}
+NCameras::~NCameras() {}
 
-void CameraRig::initInternal(){
+void NCameras::initInternal(){
   CHECK_EQ(cameras_.size(), T_C_B_.size());
   idToIndex_.clear();
   for(size_t i = 0; i < cameras_.size(); ++i) {
@@ -34,76 +34,76 @@ void CameraRig::initInternal(){
 }
 
 /// \brief get the number of cameras
-size_t CameraRig::getNumCameras() const {
+size_t NCameras::getNumCameras() const {
   return cameras_.size();
 }
 
 /// \brief get the pose of body frame with respect to the camera i
-const Transformation& CameraRig::get_T_C_B(size_t cameraIndex) const {
+const Transformation& NCameras::get_T_C_B(size_t cameraIndex) const {
   CHECK_LT(cameraIndex, cameras_.size());
   return T_C_B_[cameraIndex];
 }
 
 /// \brief get the pose of body frame with respect to the camera i
-Transformation& CameraRig::get_T_C_B_mutable(size_t cameraIndex) {
+Transformation& NCameras::get_T_C_B_mutable(size_t cameraIndex) {
   CHECK_LT(cameraIndex, cameras_.size());
   return T_C_B_[cameraIndex];
 }
 
 /// \brief set the pose of body frame with respect to the camera i
-void CameraRig::set_T_C_B(size_t cameraIndex, const Transformation& T_Ci_B) {
+void NCameras::set_T_C_B(size_t cameraIndex, const Transformation& T_Ci_B) {
   CHECK_LT(cameraIndex, T_C_B_.size());
   T_C_B_[cameraIndex] = T_Ci_B;
 }
 
 /// \brief get all transformations
-const CameraRig::TransformationVector& CameraRig::getTransformationVector() const {
+const NCameras::TransformationVector& NCameras::getTransformationVector() const {
   return T_C_B_;
 }
 
 /// \brief get the geometry object for camera i
-const Camera& CameraRig::getCamera(size_t cameraIndex) const {
+const Camera& NCameras::getCamera(size_t cameraIndex) const {
   CHECK_LT(cameraIndex, cameras_.size());
   CHECK_NOTNULL(cameras_[cameraIndex].get());
   return *cameras_[cameraIndex];
 }
 
 /// \brief get the geometry object for camera i
-Camera::Ptr CameraRig::getCameraMutable(size_t cameraIndex) {
+Camera::Ptr NCameras::getCameraMutable(size_t cameraIndex) {
   CHECK_LT(cameraIndex, cameras_.size());
   return cameras_[cameraIndex];
 }
 
 /// \brief get the geometry object for camera i
-void CameraRig::setCamera(size_t cameraIndex, Camera::Ptr camera) {
+void NCameras::setCamera(size_t cameraIndex, Camera::Ptr camera) {
   CHECK_LT(cameraIndex, cameras_.size());
   cameras_[cameraIndex] = camera;
 }
   
 /// \brief how many cameras does this system have?
-size_t CameraRig::numCameras() const {
+size_t NCameras::numCameras() const {
   return cameras_.size();
 }
 
 /// \brief get all cameras
-const std::vector<Camera::Ptr>& CameraRig::getCameraVector() const {
+const std::vector<Camera::Ptr>& NCameras::getCameraVector() const {
   return cameras_;
 }
 
 /// \brief gets the id for the camera at index i
-CameraId CameraRig::getCameraId(size_t cameraIndex) const {
+CameraId NCameras::getCameraId(size_t cameraIndex) const {
   CHECK_LT(cameraIndex, cameras_.size());
   return cameras_[cameraIndex]->getId();
 }
   
 /// \brief does this rig have a camera with this id
-bool CameraRig::hasCameraWithId(const CameraId& id) const {
+bool NCameras::hasCameraWithId(const CameraId& id) const {
   return idToIndex_.find(id) != idToIndex_.end();
 }
   
 /// \brief get the index of the camera with the id
 /// @returns -1 if the rig doesn't have a camera with this id
-size_t CameraRig::getCameraIndex(const CameraId& id) const {
+size_t NCameras::getCameraIndex(const CameraId& id) const {
   std::unordered_map<CameraId, size_t>::const_iterator it = idToIndex_.find(id);
   if(it == idToIndex_.end()) {
     return -1;
@@ -112,7 +112,7 @@ size_t CameraRig::getCameraIndex(const CameraId& id) const {
   }
 }
 
-bool CameraRig::operator==(const CameraRig& other) const {
+bool NCameras::operator==(const NCameras& other) const {
   bool same = true;
   same &= getNumCameras() == other.getNumCameras();
   same &= label_ == other.label_;
