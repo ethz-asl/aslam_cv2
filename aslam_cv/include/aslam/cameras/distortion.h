5d6f8cdcd7932ee31e2290ac37d5f459d28d1ae4
Thomas Schneider <schneith@ethz.ch>
2014-09-04 16:30:45 +0200

    Address code review comments.

--- aslam_cv/include/aslam/cameras/distortion.h
+++ aslam_cv/include/aslam/cameras/distortion.h
@@ -20,150 +20,115 @@ class Distortion {
   // TODO(slynen) Enable commented out PropertyTree support
   //  Distortion(const sm::PropertyTree& property_tree);
 
-
-//////////////////////////////////////////////////////////////////
-/** \name Constructors/destructors and operators
-*  @{
-*/
+  //////////////////////////////////////////////////////////////
+  /// \name Constructors/destructors and operators
+  /// @{
 
  protected:
   Distortion() = delete;
 
-  /// \brief Distortion base constructor. Should only be called by a specialized distortion class ctor.
-  ///
+  /// \brief Distortion base constructor.
   /// @param[in] dist_coeffs Vector containing the distortion parameters.
-  ///
   Distortion(const Eigen::VectorXd& dist_coeffs);
 
  public:
-  virtual ~Distortion();
+  virtual ~Distortion() { };
 
   /// \brief Checks for same distortion type and same parameters.
-  ///
   /// @return Same distortion?
-  ///
   virtual bool operator==(const Distortion& rhs) const;
 
-/** @} *///////////////////////////////////////////////////////////////////
+  /// @}
 
-//////////////////////////////////////////////////////////////////
-/** \name Distort methods: applies the distortion model to a point.
- *  @{
- */
+  //////////////////////////////////////////////////////////////
+  /// \name Distort methods: applies the distortion model to a point.
+  /// @{
 
   /// \brief Apply distortion to a point in the normalized image plane
-  ///
-  /// @param[in,out] y The point in the normalized image plane. After the function, this point is distorted.
-  ///
+  /// @param[in,out] point The point in the normalized image plane. After the function,
+  ///                      this point is distorted.
   void distort(Eigen::Vector2d* point) const;
 
   /// \brief Apply distortion to a point in the normalized image plane
-  ///
-  /// @param[in] y The point in the normalized image plane.
-  /// @param[out] outPoint The distorted point.
-  ///
-  void distort(const Eigen::Vector2d& point,
-               Eigen::Vector2d* out_point) const;
+  /// @param[in]  point     The point in the normalized image plane.
+  /// @param[out] out_point The distorted point.
+  void distort(const Eigen::Vector2d& point, Eigen::Vector2d* out_point) const;
 
   /// \brief Apply distortion to a point in the normalized image plane
-  ///
-  /// @param[in,out] y The point in the normalized image plane. After the function, this point is distorted.
-  /// @param[out] out_jacobian The Jacobian of the distortion function with respect to small changes in the input point.
-  ///
+  /// @param[in,out] point        The point in the normalized image plane. After the function,
+  ///                             this point is distorted.
+  /// @param[out]    out_jacobian The Jacobian of the distortion function with respect to small
+  ///                             changes in the input point.
   void distort(Eigen::Vector2d* point,
                Eigen::Matrix<double, 2, Eigen::Dynamic>* out_jacobian) const;
 
-  /// \brief Apply distortion to a point in the normalized image plane using provided distortion coefficients.
-  ///        External distortion coefficients can be specified using this function. (Ignores the internally
-  ///        stored parameters.
-  /// @param[in] dist_coeffs Vector containing the coefficients for the distortion model.
-  /// @param[in,out] y The point in the normalized image plane. After the function, this point is distorted.
-  /// @param[out] out_jacobian The Jacobian of the distortion function with respect to small changes in the input point.
-  ///                   If NULL is passed, the Jacobian calculation is skipped.
-  ///
-  virtual void distortExternalCoeffs(const Eigen::VectorXd& dist_coeffs,
-                                     Eigen::Vector2d* point,
-                                     Eigen::Matrix<double, 2, Eigen::Dynamic>* out_jacobian) const = 0;
+  /// \brief Apply distortion to a point in the normalized image plane using provided distortion
+  ///        coefficients. External distortion coefficients can be specified using this function.
+  ///        (Ignores the internally stored parameters.
+  /// @param[in]     dist_coeffs  Vector containing the coefficients for the distortion model.
+  /// @param[in,out] point        The point in the normalized image plane. After the function,
+  ///                             this point is distorted.
+  /// @param[out]    out_jacobian The Jacobian of the distortion function with respect to small
+  ///                             changes in the input point. If NULL is passed, the Jacobian
+  ///                             calculation is skipped.
+  virtual void distortUsingExternalCoefficients(const Eigen::VectorXd& dist_coeffs,
+                                 Eigen::Vector2d* point,
+                                 Eigen::Matrix<double, 2, Eigen::Dynamic>* out_jacobian) const = 0;
 
   virtual void distortParameterJacobian(const Eigen::VectorXd& dist_coeffs,
-                                        const Eigen::Vector2d& point,
-                                        Eigen::Matrix<double, 2, Eigen::Dynamic>* out_jacobian) const = 0;
+                                 const Eigen::Vector2d& point,
+                                 Eigen::Matrix<double, 2, Eigen::Dynamic>* out_jacobian) const = 0;
 
-/** @} *///////////////////////////////////////////////////////////////////
+  /// @}
 
-
-///////////////////////////////////////////////////////////////////////////
-/** \name Undistort methods: Removes the modeled distortion effects from a point.
- *  @{
- */
+  //////////////////////////////////////////////////////////////
+  /// \name Undistort methods: Removes the modeled distortion effects from a point.
+  /// @{
 
   /// \brief Apply undistortion to recover a point in the normalized image plane.
-  ///
-  /// @param[in,out] y The distorted point. After the function, this point is in the normalized image plane.
-  ///
+  /// @param[in,out] point The distorted point. After the function, this point is in
+  ///                      the normalized image plane.
   void undistort(Eigen::Vector2d* point) const;
 
   /// \brief Apply undistortion to recover a point in the normalized image plane.
-  ///
-  /// @param[in] dist_coeffs External distortion coefficients to use.
-  /// @param[in,out] y The distorted point. After the function, this point is in the normalized image plane.
-  ///
+  /// @param[in]    point     External distortion coefficients to use.
+  /// @param[out]   out_point The undistorted point in normalized image plane.
   void undistort(const Eigen::Vector2d& point, Eigen::Vector2d* out_point) const;
 
-  /// \brief Apply undistortion to recover a point in the normalized image plane.
-  ///
-  /// @param[in,out] y The distorted point. After the function, this point is in the normalized image plane.
-  /// @param[out] out_jacobian The Jacobian of the distortion function with respect to small changes in the input point.
-  ///             If NULL is passed, the Jacobian calculation is skipped.
-  ///
-  void undistort(Eigen::Vector2d* point, Eigen::Matrix<double, 2, Eigen::Dynamic>* out_jacobian) const;
 
-  /// \brief Apply undistortion to recover a point in the normalized image plane using provided distortion
-  ///        coefficients. External distortion coefficients can be specified using this function. Ignores
-  ///        the internally  stored parameters.
-  ///
-  /// @param[in] dist_coeffs Vector containing the coefficients for the distortion model.
-  /// @param[in,out] y The distorted point. After the function, this point is in the normalized image plane.
-  /// @param[out] outJy The Jacobian of the distortion function with respect to small changes in the input point.
-  ///                   If NULL is passed, the Jacobian calculation is skipped.
-  ///
-  virtual void undistortExternalCoeffs(const Eigen::VectorXd& dist_coeffs,
-                                       Eigen::Vector2d* point,
-                                       Eigen::Matrix<double, 2, Eigen::Dynamic>* out_jacobian) const = 0;
+  /// \brief Apply undistortion to recover a point in the normalized image plane using provided
+  ///        distortion coefficients. External distortion coefficients can be specified using this
+  ///        function. Ignores the internally stored parameters.
+  /// @param[in]     dist_coeffs  Vector containing the coefficients for the distortion model.
+  /// @param[in,out] point        The distorted point. After the function, this point is in the
+  ///                             normalized image plane.
+  virtual void undistortUsingExternalCoefficients(const Eigen::VectorXd& dist_coeffs,
+                                                  Eigen::Vector2d* point) const = 0;
 
-/** @} *///////////////////////////////////////////////////////////////////
+  /// @}
 
-
-///////////////////////////////////////////////////////////////////////////
-/** \name Methods to set/get distortion parameters
- *  @{
- */
+  //////////////////////////////////////////////////////////////
+  /// \name Methods to set/get distortion parameters.
+  /// @{
 
   /// \brief Set the coefficients for the distortion model.
-  ///
   /// @param[in] dist_coeffs Vector containing the coefficients.
-  ///
-  void setParameters(const Eigen::VectorXd& params);
+  void setParameters(const Eigen::VectorXd& dist_coeffs);
 
   /// \brief Get the distortion model coefficients.
-  ///
   /// @return Vector containing the coefficients.
-  ///
   Eigen::VectorXd getParameters() const;
 
   /// \brief Check the validity of distortion parameters.
-  ///
-  /// @param[in] dist_coeffs Vector containing the coefficients.
-  ///            Parameters will NOT be stored.
+  /// @param[in] dist_coeffs Vector containing the coefficients. Parameters will NOT be stored.
   /// @return If the distortion parameters are valid.
-  ///
-  virtual bool distortionParametersValid(const Eigen::VectorXd& params) const = 0;
+  virtual bool distortionParametersValid(const Eigen::VectorXd& dist_coeffs) const = 0;
   
  private:
-  /** \brief Parameter vector for the distortion model. */
-  Eigen::VectorXd dist_coeffs_;
+  /// \brief Parameter vector for the distortion model.
+  Eigen::VectorXd distortion_coefficients_;
 
-/** @} *///////////////////////////////////////////////////////////////////
+  /// @}
 
 };
 }  // namespace aslam
