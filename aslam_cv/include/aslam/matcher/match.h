759a5cc11c7bc7f67586030a2a3f08f70c966565
Mike Bosse <mike.bosse@mavt.ethz.ch>
2014-09-19 13:25:34 +0200

    applied style formatting

--- aslam_cv/include/aslam/matcher/match.h
+++ aslam_cv/include/aslam/matcher/match.h
@@ -9,25 +9,36 @@ namespace aslam {
 template<typename Score>
 struct Match {
   /// \brief Initialize to an invalid match.
-  Match() : correspondence{ -1, -1 }, score(static_cast<Score>(0)) {}
+  Match()
+      : correspondence { -1, -1 },
+        score(static_cast<Score>(0)) {
+  }
 
   /// \brief Initialize with correspondences and a score.
-  Match(int correspondence_a, int correspondence_b, Score score) :
-    correspondence{correspondence_a, correspondence_b}, score(score) {}
+  Match(int correspondence_a, int correspondence_b, Score score)
+      : correspondence { correspondence_a, correspondence_b },
+        score(score) {
+  }
 
-    /// \brief Get the index into list A.
-  int getIndexA()  const { return correspondence[0]; }
+  /// \brief Get the index into list A.
+  int getIndexA() const {
+    return correspondence[0];
+  }
 
   /// \brief Get the index into list B.
-  int getIndexB()  const { return correspondence[1]; }
+  int getIndexB() const {
+    return correspondence[1];
+  }
 
   /// \brief Get the score given to the match.
-  Score getScore() const { return score; }
+  Score getScore() const {
+    return score;
+  }
 
-  bool operator<( const Match &other) const {
+  bool operator<(const Match &other) const {
     return this->score < other.score;
   }
-  bool operator>( const Match &other) const {
+  bool operator>(const Match &other) const {
     return this->score > other.score;
   }
 
