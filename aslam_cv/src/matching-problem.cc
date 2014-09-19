5f01594c14098973e0ba962bf36a7df131c70ad8
Mike Bosse <mike.bosse@mavt.ethz.ch>
2014-09-19 09:58:19 +0200

    template class .cc file useless

--- aslam_cv/src/matching-problem.cc
+++
@@ -1,9 +0,0 @@
-
-#include "aslam/matcher/matching-problem.h"
-
-namespace aslam {
-
-template <typename SCORE> MatchingProblem<SCORE>::MatchingProblem() {}
-template <typename SCORE> MatchingProblem<SCORE>::~MatchingProblem() {}
-
-}
