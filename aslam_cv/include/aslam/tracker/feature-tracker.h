4047182b757f7447379c46e7a8d2a169c7b66049
Thomas Schneider <schneith@ethz.ch>
2014-10-03 16:39:55 +0200

    Update documentation and format some stuff.

--- aslam_cv/include/aslam/tracker/feature-tracker.h
+++ aslam_cv/include/aslam/tracker/feature-tracker.h
@@ -13,10 +13,9 @@
 // Some forward declarations.
 namespace aslam { class VisualFrame; }
 
-
 namespace aslam {
 /// \class FeatureTracker
-/// \brief Feature tracker base class defining the interface for feature trackers.
+/// \brief Base class defining the interface for feature trackers and providing visualization.
 class FeatureTracker {
  public:
   ASLAM_POINTER_TYPEDEFS(FeatureTracker);
@@ -45,6 +44,18 @@ class FeatureTracker {
   virtual void addFrame(std::shared_ptr<VisualFrame> current_frame_ptr,
                         const Eigen::Matrix3d& C_current_prev) = 0;
 
+  /// \brief Add a new VisualFrame to the tracker and draw tracks to an image.
+  ///        The frame needs to contain KeypointMeasurements and Descriptors channel. Usually
+  ///        this VisualFrame will be an output of a VisualPipeline. The tracker will fill the
+  ///        TrackId channel with the tracking results.
+  ///        NOTE: Make sure the frames arrive in correct time ordering.
+  /// @param[in] current_frame_ptr Shared pointer to the frame to process. The frame needs to
+  ///                              contain KeypointMeasurements and Descriptors channel. Usually
+  ///                              this VisualFrame will be an output of a VisualPipeline.
+  /// @param[in] C_current_prev    Rotation matrix that defines the camera rotation between the
+  ///                              current and the previous frame passed to the tracker. For e.g.
+  ///                              this could be the output of the gyro/odometry integrators.
+  /// @param[out] track_image      Tracks are drawn into this image.
   void addFrameAndDrawTracks(std::shared_ptr<VisualFrame> current_frame_ptr,
                              const Eigen::Matrix3d& C_current_prev,
                              cv::Mat* track_image) {
@@ -52,8 +63,11 @@ class FeatureTracker {
     drawTracks(current_frame_ptr, track_image);
   }
 
-  void drawTracks(std::shared_ptr<VisualFrame> current_frame_ptr,
-                  cv::Mat* track_image);
+  /// \brief Draw feature tracks to an image.
+  /// @param[in] current_frame_ptr Shared pointer to the frame to process. The frame needs to
+  ///                              contain a TrackId channel.
+  /// @param[out] track_image      Tracks are drawn into this image.
+  void drawTracks(std::shared_ptr<VisualFrame> current_frame_ptr, cv::Mat* track_image);
 
   /// Return the used camera model.
   const aslam::Camera& getCamera() const { return *CHECK_NOTNULL(camera_.get()); };
@@ -66,6 +80,6 @@ class FeatureTracker {
   unsigned int current_track_id_;
 };
 
-}       // namespace aslam
+} // namespace aslam
 
 #endif  // ASLAM_FEATURE_TRACKER_BASE_H_
