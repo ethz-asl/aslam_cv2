63d3bfe26ba151381cf64777dfa1820aeb944f30
Thomas Schneider <schneith@ethz.ch>
2014-10-26 01:03:09 +0200

    Address code review comments.

--- aslam_cv/test/test-triangulation.cc
+++ aslam_cv/test/test-triangulation.cc
@@ -13,11 +13,11 @@ const size_t kNumObservations = 20;
 
 void fillObservations(
     size_t n_observations,
-    const aslam::Transformation& T_I_C,
+    const aslam::Transformation& T_B_C,
     aslam::Aligned<std::vector, Eigen::Vector2d>::type* measurements,
-    aslam::Aligned<std::vector, aslam::Transformation>::type* T_G_I) {
+    aslam::Aligned<std::vector, aslam::Transformation>::type* T_W_B) {
   CHECK_NOTNULL(measurements);
-  CHECK_NOTNULL(T_G_I);
+  CHECK_NOTNULL(T_W_B);
 
   Eigen::Vector3d position_start(-2,-2,-1);
   Eigen::Vector3d position_end(2,2,1);
@@ -27,12 +27,12 @@ void fillObservations(
   // Move along line from position_start to position_end.
   for(size_t i = 0; i < n_observations; ++i) {
     Eigen::Vector3d test_pos(position_start + i * position_step);
-    aslam::Transformation T_G_I_current(test_pos, Eigen::Quaterniond::Identity());
-    T_G_I->push_back(T_G_I_current);
+    aslam::Transformation T_W_B_current(test_pos, Eigen::Quaterniond::Identity());
+    T_W_B->push_back(T_W_B_current);
 
-    aslam::Transformation T_C_G = T_I_C.inverted() * T_G_I_current.inverted();
+    aslam::Transformation T_C_W = T_B_C.inverted() * T_W_B_current.inverted();
 
-    Eigen::Vector3d C_landmark = T_C_G.transform(kGPoint);
+    Eigen::Vector3d C_landmark = T_C_W.transform(kGPoint);
     Eigen::Vector2d measurement = C_landmark.head<2>() / C_landmark[2];
     measurements->push_back(measurement);
   }
@@ -41,21 +41,21 @@ void fillObservations(
 class TriangulationTest : public testing::Test {
  protected:
   virtual void SetUp() {
-    T_I_C_.setIdentity();
+    T_B_C_.setIdentity();
   }
 
-  aslam::Transformation T_I_C_;
+  aslam::Transformation T_B_C_;
 };
 
 TEST_F(TriangulationTest, LinearTriangulateFromNViews) {
   aslam::Aligned<std::vector, Eigen::Vector2d>::type measurements;
-  aslam::Aligned<std::vector, aslam::Transformation>::type T_G_I;
-  Eigen::Vector3d G_point;
+  aslam::Aligned<std::vector, aslam::Transformation>::type T_W_B;
+  Eigen::Vector3d W_point;
 
-  fillObservations(kNumObservations, T_I_C_, &measurements, &T_G_I);
-  aslam::linearTriangulateFromNViews(measurements, T_G_I, T_I_C_, &G_point);
+  fillObservations(kNumObservations, T_B_C_, &measurements, &T_W_B);
+  aslam::linearTriangulateFromNViews(measurements, T_W_B, T_B_C_, &W_point);
 
-  EXPECT_TRUE(EIGEN_MATRIX_NEAR(kGPoint, G_point, kDoubleTolerance));
+  EXPECT_TRUE(EIGEN_MATRIX_NEAR(kGPoint, W_point, kDoubleTolerance));
 }
 
 
