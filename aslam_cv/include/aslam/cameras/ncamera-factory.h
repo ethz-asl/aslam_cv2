840ff6e73a1f05ba06e243ee172936508d4f451c
Mathias Buerki <mathias.buerki@mavt.ethz.ch>
2014-10-28 20:26:08 +0100

    added the ncamera factory

---
+++ aslam_cv/include/aslam/cameras/ncamera-factory.h
@@ -0,0 +1,14 @@
+#ifndef ASLAM_NCAMERA_FACTORY_H_
+#define ASLAM_NCAMERA_FACTORY_H_
+
+#include <aslam/cameras/ncamera.h>
+
+namespace aslam {
+
+  /// \brief Creates an artificial 4-camera rig in a plane with  a camera pointing in each direction.
+  ///        This is similar to the V-Charge or JanETH camera system.
+  aslam::NCamera::Ptr createPlanar4CameraRig();
+
+}  // namespace aslam
+
+#endif /* NCAMERA_FACTORY_H_ */
