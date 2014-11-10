6b9b8d9d9ce5f0363becf4d14fe9cc13c142157f
Titus Cieslewski <dev@titus-c.ch>
2014-11-10 18:54:57 +0100

    Macrified id types.

--- aslam_cv/include/aslam/common/unique-id.h
+++ aslam_cv/include/aslam/common/unique-id.h
@@ -1,32 +1,22 @@
 #ifndef ASLAM_COMMON_UNIQUE_ID_H_
 #define ASLAM_COMMON_UNIQUE_ID_H_
+
 #include <sm/hash_id.hpp>
 
 namespace aslam {
-class FrameId : public sm::HashId {
- public:
-  FrameId() = default;
-};
 
-class NFramesId : public sm::HashId {
- public:
-  NFramesId() = default;
-};
+// TODO(tcies) duplicate with map api unique id... what to do?
+#define ASLAM_UNIQUE_ID(IdType) \
+class IdType : public sm::HashId { \
+ public: \
+  IdType() = default; \
+}
 
-class CameraId : public sm::HashId {
- public:
-  CameraId() = default;
-};
-
-class NCameraId : public sm::HashId {
- public:
-  NCameraId() = default;
-};
-
-class LandmarkId : public sm::HashId {
- public:
-  LandmarkId() = default;
-};
+ASLAM_UNIQUE_ID(FrameId);
+ASLAM_UNIQUE_ID(NFramesId);
+ASLAM_UNIQUE_ID(CameraId);
+ASLAM_UNIQUE_ID(NCameraId);
+ASLAM_UNIQUE_ID(LandmarkId);
 
 }  // namespace aslam
 
