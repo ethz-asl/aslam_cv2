c45f44a84d5baf6ce0d2590d699f89bd8556277c
Mathias Buerki <mathias.buerki@mavt.ethz.ch>
2015-01-02 18:13:49 +0100

    renamed constants

--- aslam_cv/src/triangulation.cc
+++ aslam_cv/src/triangulation.cc
@@ -3,12 +3,12 @@
 namespace aslam {
 
 TriangulationResult::Status TriangulationResult::SUCCESSFUL =
-    TriangulationResult::Status::SUCCESSFUL;
+    TriangulationResult::Status::kSuccessful;
 TriangulationResult::Status TriangulationResult::TOO_FEW_MEASUREMENTS =
-    TriangulationResult::Status::TOO_FEW_MEASUREMENTS;
+    TriangulationResult::Status::kTooFewMeasurments;
 TriangulationResult::Status TriangulationResult::UNOBSERVABLE =
-    TriangulationResult::Status::UNOBSERVABLE;
+    TriangulationResult::Status::kUnobservable;
 TriangulationResult::Status TriangulationResult::UNINITIALIZED =
-    TriangulationResult::Status::UNINITIALIZED;
+    TriangulationResult::Status::kUninitialized;
 }  // namespace aslam
 
