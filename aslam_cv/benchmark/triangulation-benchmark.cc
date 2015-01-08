ba24dd0d623436ccad154714a26e77262381e416
Titus Cieslewski <dev@titus-c.ch>
2015-01-08 15:20:00 +0100

    Addr. comments of #239

--- aslam_cv/benchmark/triangulation-benchmark.cc
+++ aslam_cv/benchmark/triangulation-benchmark.cc
@@ -5,6 +5,7 @@
 void sampleXYPlaneSine(const double x_min, const double x_max,
                        const size_t num_samples, Eigen::Matrix3Xd* result) {
   CHECK_NOTNULL(result)->resize(3, num_samples);
+  CHECK_GT(num_samples, 1);
   for (size_t i = 0; i < num_samples; ++i) {
     double x = x_min + i * (x_max - x_min) / (num_samples - 1);
     result->block<3, 1>(0, i) << x, sin(x), 0;
@@ -31,6 +32,7 @@ constexpr size_t kMaxNumMeasurements = 50;
 template <>
 void plotIfDone<Eigen::Matrix3Xd>() {
   FILE* plot = popen("gnuplot --persist", "w");
+  fprintf(plot, "set logscale y\n");
   fprintf(plot, "plot '-' w l, '-' w l\n");
   for (size_t i = 2; i <= kMaxNumMeasurements; ++i) {
     fprintf(plot, "%u %lf\n", i, sm::timing::Timing::getMeanSeconds(
@@ -49,6 +51,7 @@ TYPED_TEST(TriangulationFixture, Performance) {
   constexpr double kDepth = 5;
   constexpr size_t num_samples = 10;
   this->p_W_L_ << 1, 1, kDepth;
+  CHECK_GT(kMaxNumMeasurements, 1);
   for (size_t num_measurements = 2; num_measurements <= kMaxNumMeasurements;
       ++num_measurements) {
     this->setNMeasurements(num_measurements);
