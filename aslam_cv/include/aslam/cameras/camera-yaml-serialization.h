0777d8efe8f5d9dc63b509b4787381f5fffe9e8b
Mathias Buerki <mathias.buerki@mavt.ethz.ch>
2015-01-09 12:42:05 +0100

    added header for ncam

--- aslam_cv/include/aslam/cameras/camera-yaml-serialization.h
+++ aslam_cv/include/aslam/cameras/camera-yaml-serialization.h
@@ -6,6 +6,7 @@
 
 namespace aslam {
 class Camera;
+class NCamera;
 }  // namespace aslam
 
 namespace YAML {
@@ -19,6 +20,17 @@ struct convert<std::shared_ptr<aslam::Camera>> {
   static bool decode(const Node& node, std::shared_ptr<aslam::Camera>& camera);
   static Node encode(const std::shared_ptr<aslam::Camera>& camera);
 };
+
+template<>
+struct convert<std::shared_ptr<aslam::NCamera>> {
+  /// This function will attempt to parse a camera from the yaml node.
+  /// By default, yaml-cpp will throw and exception if the parsing fails.
+  /// This function was written to *not* throw exceptions. Hence, decode always
+  /// returns true, but when it fails, the shared pointer will be null.
+  static bool decode(const Node& node, std::shared_ptr<aslam::NCamera>& ncamera);
+  static Node encode(const std::shared_ptr<aslam::NCamera>& ncamera);
+};
+
 
 }  // namespace YAML
 
