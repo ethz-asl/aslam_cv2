894b50dc568d9e0776e13a8b98d9095c5dac35a4
Mathias Buerki <mathias.buerki@mavt.ethz.ch>
2015-01-09 17:42:04 +0100

    spelling

--- aslam_cv/include/aslam/cameras/yaml/ncamera-yaml-serialization.h
+++ aslam_cv/include/aslam/cameras/yaml/ncamera-yaml-serialization.h
@@ -12,14 +12,13 @@ namespace YAML {
 
 template<>
 struct convert<std::shared_ptr<aslam::NCamera>> {
-  /// This function will attempt to parse a camera from the yaml node.
+  /// This function will attempt to parse an ncamera from the yaml node.
   /// By default, yaml-cpp will throw and exception if the parsing fails.
   /// This function was written to *not* throw exceptions. Hence, decode always
   /// returns true, but when it fails, the shared pointer will be null.
   static bool decode(const Node& node, std::shared_ptr<aslam::NCamera>& ncamera);
   static Node encode(const std::shared_ptr<aslam::NCamera>& ncamera);
 };
-
 
 }  // namespace YAML
 
