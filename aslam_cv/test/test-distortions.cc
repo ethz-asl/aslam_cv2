7599ae7441a883d35da1b2520b890a4c70055a44
Paul Furgale <paul.furgale@gmail.com>
2015-01-15 14:30:41 +0100

    Fixed some unit tests. Still one failing

--- aslam_cv/test/test-distortions.cc
+++ aslam_cv/test/test-distortions.cc
@@ -178,8 +178,6 @@ TEST(TestParameter, testEquidistantDistortionParameters) {
   Eigen::Vector4d valid = Eigen::Vector4d::Zero();
   EXPECT_TRUE(aslam::EquidistantDistortion::areParametersValid(valid));
 
-  Eigen::Vector4d valid_null;
-  EXPECT_TRUE(aslam::NullDistortion::areParametersValid(valid_null));
 }
 
 namespace aslam {
