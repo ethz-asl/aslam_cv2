51acba3eb60d884d5ae4fdf25489e557e8bb1928
Thomas Schneider <schneith@ethz.ch>
2015-02-03 19:13:10 +0100

    more

--- aslam_cv/include/aslam/matcher/matching-engine.h
+++ aslam_cv/include/aslam/matcher/matching-engine.h
@@ -20,7 +20,7 @@ class MatchingEngine {
 
   virtual bool match(MatchingProblem* problem, MatchesWithScore* matches_A_B) = 0;
 
-  bool match(MatchingProblem* problem, Matches* matches_A_B) {
+  virtual bool match(MatchingProblem* problem, Matches* matches_A_B) {
     CHECK_NOTNULL(problem);
     CHECK_NOTNULL(matches_A_B);
     MatchesWithScore matches_with_score_A_B;
