21e1dde0c8a2d2676b93c273e791b4354b0ea63a
Thomas Schneider <schneith@ethz.ch>
2015-04-23 14:49:26 +0200

    Create aslam_gv package.

---
+++ aslam_cv/aslam_cv_geometric_vision/include/aslam/geometric_vision/match-outlier-rejection-twopt.h
@@ -0,0 +1,33 @@
+#ifndef ASLAM_MATCH_OUTLIER_REJECTION_TWOPT_H_
+#define ASLAM_MATCH_OUTLIER_REJECTION_TWOPT_H_
+
+#include <Eigen/Dense>
+#include <glog/logging.h>
+
+namespace aslam {
+class Quaternion;
+class VisualFrame;
+struct MatchesWithScore;
+
+/// \brief Runs a 2-pt RANSAC scheme over the specified match list to separate matches into
+///        out-/inliers. The bearing vectors are unrotated using a given rotation q_Ckp1_Ck
+///        and a model is used that only estimates the translational motion.
+/// @param[in]  frame_kp1  Current frame.
+/// @param[in]  frame_k    Previous frame.
+/// @param[in]  q_Ckp1_Ck  Rotation taking points from the camera frame k to the camera frame k+1.
+/// @param[in]  matches_kp1_k The matches between the frames.
+/// @param[in]  ransac_threshold  RANSAC threshold to consider a sample as inlier.
+///                               The threshold is defined as:  1 - cos(max_ray_disparity_angle).
+/// @param[in]  ransac_max_iterations Max. RANSAC iterations.
+/// @param[out] inlier_matches_kp1_k The list of inlier matches.
+/// @param[out] outlier_matches_kp1_k The list of outlier matches.
+/// @return RANSAC successful?
+bool rejectOutlierKeypointMatchesTwopt(const aslam::VisualFrame& frame_kp1,
+                                       const aslam::VisualFrame& frame_k,
+                                       const aslam::Quaternion& q_Ckp1_Ck,
+                                       const aslam::MatchesWithScore& matches_kp1_k,
+                                       double ransac_threshold, size_t ransac_max_iterations,
+                                       aslam::MatchesWithScore* inlier_matches_kp1_k,
+                                       aslam::MatchesWithScore* outlier_matches_kp1_k);
+}  // namespace aslam
+#endif  // ASLAM_MATCH_OUTLIER_REJECTION_TWOPT_H_
