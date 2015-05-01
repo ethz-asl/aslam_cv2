9c49446030b84921cfce5b0ed98b41b01275509d
Thomas Schneider <schneith@ethz.ch>
2015-05-02 01:29:28 +0200

    cr + fix test.

--- aslam_cv/aslam_cv_pipeline/test/test-visual-npipeline.cc
+++ aslam_cv/aslam_cv_pipeline/test/test-visual-npipeline.cc
@@ -21,7 +21,6 @@ class VisualNPipelineTest : public ::testing::Test {
   void constructNCamera(unsigned num_cameras,
                         unsigned num_threads,
                         int64_t timestamp_tolerance_ns) {
-
     NCameraId id;
     id.randomize();
     Aligned<std::vector, kindr::minimal::QuatTransformation>::type T_C_B;
@@ -140,11 +139,11 @@ TEST_F(VisualNPipelineTest, testTimestampDiff) {
 
   pipeline_->processImage(1, getImageFromCamera(1), 101);
   pipeline_->waitForAllWorkToComplete();
-  ASSERT_EQ(0u, pipeline_->getNumFramesComplete());
-  ASSERT_EQ(2u, pipeline_->getNumFramesProcessing());
+  ASSERT_EQ(2u, pipeline_->getNumFramesProcessing());  // 0, 101
+  ASSERT_EQ(0u, pipeline_->getNumFramesComplete());    // -
 
   // Get the latest. This should be null as no frames are complete.
-  nframes = pipeline_->getLatestAndClear();
+  nframes = pipeline_->getNext();
   ASSERT_TRUE(nframes.get() == NULL);
   // And there should still be two processing
   ASSERT_EQ(2u, pipeline_->getNumFramesProcessing());
@@ -152,31 +151,59 @@ TEST_F(VisualNPipelineTest, testTimestampDiff) {
   // Add an even later frame
   pipeline_->processImage(0, getImageFromCamera(0), 401);
   pipeline_->waitForAllWorkToComplete();
-  ASSERT_EQ(3u, pipeline_->getNumFramesProcessing());
-  ASSERT_EQ(0u, pipeline_->getNumFramesComplete());
+  ASSERT_EQ(3u, pipeline_->getNumFramesProcessing());  // 0, 101, 401
+  ASSERT_EQ(0u, pipeline_->getNumFramesComplete());    // -
 
-  // Finish the middle frame
+  // Finish the middle frame, but it should still not be moved to the output queue.
+  // As there are nframes still processing with lower timestamps and the output is
+  // chronologically ordered. This leaves us still with 3 processing and 0 completed.
   pipeline_->processImage(0, getImageFromCamera(0), 101);
   pipeline_->waitForAllWorkToComplete();
-  ASSERT_EQ(2u, pipeline_->getNumFramesProcessing());
-  ASSERT_EQ(1u, pipeline_->getNumFramesComplete());
-  // This should clear the oldest unfinished frame from the processing queue
-  nframes = pipeline_->getLatestAndClear();
+  ASSERT_EQ(3u, pipeline_->getNumFramesProcessing());  // 0, 101, 401
+  ASSERT_EQ(0u, pipeline_->getNumFramesComplete());    // -
+
+  // Get the latest. This should be null as no frames are complete.
+  nframes = pipeline_->getNext();
+  ASSERT_TRUE(nframes.get() == NULL);
+
+  // Finish the first queued frame. This should leave us with two frames (0, 101) in the
+  // output queue.
+  pipeline_->processImage(1, getImageFromCamera(1), 0);
+  pipeline_->waitForAllWorkToComplete();
+  ASSERT_EQ(1u, pipeline_->getNumFramesProcessing());  // 401
+  ASSERT_EQ(2u, pipeline_->getNumFramesComplete());    // 0, 101
+
+  // Check output queue content and ordering.
+  nframes = pipeline_->getNext();
+  ASSERT_TRUE(nframes.get() != NULL);
+  ASSERT_EQ(0, nframes->getFrame(0).getTimestampNanoseconds());
+  ASSERT_EQ(0, nframes->getFrame(1).getTimestampNanoseconds());
+
+  nframes = pipeline_->getNext();
   ASSERT_TRUE(nframes.get() != NULL);
   ASSERT_EQ(101, nframes->getFrame(0).getTimestampNanoseconds());
   ASSERT_EQ(101, nframes->getFrame(1).getTimestampNanoseconds());
-  ASSERT_EQ(1u, pipeline_->getNumFramesProcessing());
-  ASSERT_EQ(0u, pipeline_->getNumFramesComplete());
 
-  // Finish the last processing frame
+  // The queue should now be empty.
+  ASSERT_EQ(1u, pipeline_->getNumFramesProcessing());  // 401
+  ASSERT_EQ(0u, pipeline_->getNumFramesComplete());    // -
+
+  // Now we finish the last frame and check it.
   pipeline_->processImage(1, getImageFromCamera(1), 401);
   pipeline_->waitForAllWorkToComplete();
-  ASSERT_EQ(0u, pipeline_->getNumFramesProcessing());
-  ASSERT_EQ(1u, pipeline_->getNumFramesComplete());
-  nframes = pipeline_->getLatestAndClear();
+  ASSERT_EQ(0u, pipeline_->getNumFramesProcessing());  // -
+  ASSERT_EQ(1u, pipeline_->getNumFramesComplete());    // 401
+
+  nframes = pipeline_->getNext();
   ASSERT_TRUE(nframes.get() != NULL);
   ASSERT_EQ(401, nframes->getFrame(0).getTimestampNanoseconds());
   ASSERT_EQ(401, nframes->getFrame(1).getTimestampNanoseconds());
+
+  ASSERT_EQ(0u, pipeline_->getNumFramesProcessing());
+  ASSERT_EQ(0u, pipeline_->getNumFramesComplete());
+
+  nframes = pipeline_->getNext();
+  ASSERT_TRUE(nframes.get() == NULL);
 }
 
 ASLAM_UNITTEST_ENTRYPOINT
