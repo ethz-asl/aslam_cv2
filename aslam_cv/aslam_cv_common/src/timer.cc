46804acd1a642dc66f5201b6665e49d7b6f918f4
Thomas Schneider <schneith@ethz.ch>
2015-07-19 19:23:50 +0200

    Code review.

--- aslam_cv/aslam_cv_common/src/timer.cc
+++ aslam_cv/aslam_cv_common/src/timer.cc
@@ -17,26 +17,26 @@ Timing& Timing::Instance() {
   return t;
 }
 
-Timing::Timing() : maxTagLength_(0) { }
+Timing::Timing() : max_tag_length_(0u) { }
 
 Timing::~Timing() { }
 
 // Static functions to query the timers:
-size_t Timing::GetHandle(std::string const& tag) {
+size_t Timing::GetHandle(const std::string& tag) {
   std::lock_guard<std::mutex> lock(Instance().mutex_);
   // Search for an existing tag.
-  map_t::iterator i = Instance().tagMap_.find(tag);
-  if (i == Instance().tagMap_.end()) {
+  map_t::iterator tag_iterator = Instance().tag_map_.find(tag);
+  if (tag_iterator == Instance().tag_map_.end()) {
     // If it is not there, create a tag.
     size_t handle = Instance().timers_.size();
-    Instance().tagMap_[tag] = handle;
+    Instance().tag_map_[tag] = handle;
     Instance().timers_.push_back(statistics::StatisticsMapValue());
     // Track the maximum tag length to help printing a table of timing values
     // later.
-    Instance().maxTagLength_ = std::max(Instance().maxTagLength_, tag.size());
+    Instance().max_tag_length_ = std::max(Instance().max_tag_length_, tag.size());
     return handle;
   } else {
-    return i->second;
+    return tag_iterator->second;
   }
 }
 
@@ -45,7 +45,7 @@ std::string Timing::GetTag(size_t handle) {
   std::string tag;
 
   // Perform a linear search for the tag.
-  for (typename map_t::value_type current_tag : Instance().tagMap_) {
+  for (typename map_t::value_type current_tag : Instance().tag_map_) {
     if (current_tag.second == handle) {
       return current_tag.first;
     }
@@ -54,40 +54,42 @@ std::string Timing::GetTag(size_t handle) {
 }
 
 // Class functions used for timing.
-Timer::Timer(std::string const& tag, bool construct_stopped)
-    : timing_(false), handle_(Timing::GetHandle(tag)), tag_(tag) {
-  if (!construct_stopped)
+Timer::Timer(const std::string& tag, bool construct_stopped)
+    : is_timing_(false), handle_(Timing::GetHandle(tag)), tag_(tag) {
+  if (!construct_stopped) {
     Start();
+  }
 }
 
 Timer::~Timer() {
-  if (IsTiming())
+  if (IsTiming()) {
     Stop();
+  }
 }
 
 void Timer::Start() {
-  timing_ = true;
+  is_timing_ = true;
   time_ = std::chrono::system_clock::now();
 }
 
 double Timer::Stop() {
-  if (timing_) {
+  if (is_timing_) {
     std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
     double dt = static_cast<double>(std::chrono::duration_cast<std::chrono::nanoseconds>(
         now - time_).count()) * kNumSecondsPerNanosecond;
     Timing::Instance().AddTime(handle_, dt);
-    timing_ = false;
+    is_timing_ = false;
     return dt;
   }
   return 0.0;
 }
 
 void Timer::Discard() {
-  timing_ = false;
+  is_timing_ = false;
 }
 
 bool Timer::IsTiming() const {
-  return timing_;
+  return is_timing_;
 }
 
 size_t Timer::GetHandle() const {
@@ -104,7 +106,7 @@ double Timing::GetTotalSeconds(size_t handle) {
   return Instance().timers_[handle].Sum();
 }
 
-double Timing::GetTotalSeconds(std::string const& tag) {
+double Timing::GetTotalSeconds(const std::string& tag) {
   return GetTotalSeconds(GetHandle(tag));
 }
 
@@ -113,7 +115,7 @@ double Timing::GetMeanSeconds(size_t handle) {
   return Instance().timers_[handle].Mean();
 }
 
-double Timing::GetMeanSeconds(std::string const& tag) {
+double Timing::GetMeanSeconds(const std::string& tag) {
   return GetMeanSeconds(GetHandle(tag));
 }
 
@@ -122,7 +124,7 @@ size_t Timing::GetNumSamples(size_t handle) {
   return Instance().timers_[handle].TotalSamples();
 }
 
-size_t Timing::GetNumSamples(std::string const& tag) {
+size_t Timing::GetNumSamples(const std::string& tag) {
   return GetNumSamples(GetHandle(tag));
 }
 
@@ -131,7 +133,7 @@ double Timing::GetVarianceSeconds(size_t handle) {
   return Instance().timers_[handle].LazyVariance();
 }
 
-double Timing::GetVarianceSeconds(std::string const& tag) {
+double Timing::GetVarianceSeconds(const std::string& tag) {
   return GetVarianceSeconds(GetHandle(tag));
 }
 
@@ -140,7 +142,7 @@ double Timing::GetMinSeconds(size_t handle) {
   return Instance().timers_[handle].Min();
 }
 
-double Timing::GetMinSeconds(std::string const& tag) {
+double Timing::GetMinSeconds(const std::string& tag) {
   return GetMinSeconds(GetHandle(tag));
 }
 
@@ -149,7 +151,7 @@ double Timing::GetMaxSeconds(size_t handle) {
   return Instance().timers_[handle].Max();
 }
 
-double Timing::GetMaxSeconds(std::string const& tag) {
+double Timing::GetMaxSeconds(const std::string& tag) {
   return GetMaxSeconds(GetHandle(tag));
 }
 
@@ -158,7 +160,7 @@ double Timing::GetHz(size_t handle) {
   return 1.0 / Instance().timers_[handle].RollingMean();
 }
 
-double Timing::GetHz(std::string const& tag) {
+double Timing::GetHz(const std::string& tag) {
   return GetHz(GetHandle(tag));
 }
 
@@ -177,36 +179,36 @@ void Timing::Print(std::ostream& out) {  //NOLINT
   map_t tagMap;
   {
     std::lock_guard<std::mutex> lock(Instance().mutex_);
-    tagMap = Instance().tagMap_;
+    tagMap = Instance().tag_map_;
   }
 
   if (tagMap.empty()) {
     return;
   }
 
-  out << "SM Timing\n";
-  out << "-----------\n";
+  out << "SM Timing" << std::endl;
+  out << "-----------" << std::endl;
   for (typename map_t::value_type t : tagMap) {
-    size_t i = t.second;
-    out.width((std::streamsize) Instance().maxTagLength_);
+    size_t time_i = t.second;
+    out.width((std::streamsize) Instance().max_tag_length_);
     out.setf(std::ios::left, std::ios::adjustfield);
     out << t.first << "\t";
     out.width(7);
 
     out.setf(std::ios::right, std::ios::adjustfield);
-    out << GetNumSamples(i) << "\t";
-    if (GetNumSamples(i) > 0) {
-      out << SecondsToTimeString(GetTotalSeconds(i)) << "\t";
-      double meansec = GetMeanSeconds(i);
-      double stddev = sqrt(GetVarianceSeconds(i));
+    out << GetNumSamples(time_i) << "\t";
+    if (GetNumSamples(time_i) > 0) {
+      out << SecondsToTimeString(GetTotalSeconds(time_i)) << "\t";
+      double meansec = GetMeanSeconds(time_i);
+      double stddev = sqrt(GetVarianceSeconds(time_i));
       out << "(" << SecondsToTimeString(meansec) << " +- ";
       out << SecondsToTimeString(stddev) << ")\t";
 
-      double minsec = GetMinSeconds(i);
-      double maxsec = GetMaxSeconds(i);
+      double min_sec = GetMinSeconds(time_i);
+      double max_sec = GetMaxSeconds(time_i);
 
       // The min or max are out of bounds.
-      out << "[" << SecondsToTimeString(minsec) << "," << SecondsToTimeString(maxsec) << "]";
+      out << "[" << SecondsToTimeString(min_sec) << "," << SecondsToTimeString(max_sec) << "]";
     }
     out << std::endl;
   }
@@ -220,7 +222,7 @@ std::string Timing::Print() {
 
 void Timing::Reset() {
   std::lock_guard<std::mutex> lock(Instance().mutex_);
-  Instance().tagMap_.clear();
+  Instance().tag_map_.clear();
 }
 
 }  // namespace timing
