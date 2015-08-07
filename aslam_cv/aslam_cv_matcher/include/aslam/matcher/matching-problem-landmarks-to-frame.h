83a22f68d8c9cf6cacfea0ab041fff0a7a1efc04
Mathias Buerki <mathias.buerki@mavt.ethz.ch>
2015-08-07 14:31:53 +0200

    landmark to frame matching problem and unit tests

---
+++ aslam_cv/aslam_cv_matcher/include/aslam/matcher/matching-problem-landmarks-to-frame.h
@@ -0,0 +1,172 @@
+#ifndef ASLAM_CV_MATCHING_PROBLEM_LANDMARKS_TO_FRAME_H_
+#define ASLAM_CV_MATCHING_PROBLEM_LANDMARKS_TO_FRAME_H_
+
+/// \addtogroup Matching
+/// @{
+///
+/// @}
+
+#include <map>
+#include <memory>
+#include <vector>
+
+#include <aslam/common/macros.h>
+#include <aslam/common/pose-types.h>
+#include <aslam/common/feature-descriptor-ref.h>
+#include <Eigen/Core>
+
+#include "aslam/matcher/matching-problem.h"
+#include "match.h"
+
+namespace aslam {
+class VisualFrame;
+
+typedef Eigen::Matrix<unsigned char, Eigen::Dynamic, 1> Descriptor;
+
+struct LandmarkWithDescriptor {
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  LandmarkWithDescriptor() = delete;
+  LandmarkWithDescriptor(const Eigen::Vector3d& t_G_C,
+                         const Descriptor& descriptor)
+    : t_G_C_(t_G_C), descriptor_(descriptor) {}
+  virtual ~LandmarkWithDescriptor() = default;
+
+  const Eigen::Vector3d& get_t_G_C() const {
+    return t_G_C_;
+  }
+
+  const Descriptor& getDescriptor() const {
+    return descriptor_;
+  }
+ private:
+  Eigen::Vector3d t_G_C_;
+  Descriptor descriptor_;
+};
+
+typedef Aligned<std::vector, LandmarkWithDescriptor>::type LandmarkWithDescriptorList;
+
+/// \class MatchingProblem
+/// \brief Defines the specifics of a matching problem.
+/// The problem is assumed to have a list of landmarks (3D points + descriptors)
+/// and a visual frame with keypoints and descriptors. The landmarks are then
+/// matched against the keypoints in the frame based on image space distance
+/// and descriptor distance. The landmarks are expected to be expressed in the
+/// camera frame of the visual frame.
+///
+/// Coordinate Frames:
+///   C: camera frame of the visual frame.
+class MatchingProblemLandmarksToFrame : public MatchingProblem {
+public:
+  ASLAM_POINTER_TYPEDEFS(MatchingProblemLandmarksToFrame);
+  ASLAM_DISALLOW_EVIL_CONSTRUCTORS(MatchingProblemLandmarksToFrame);
+  friend class MatcherTest;
+
+  MatchingProblemLandmarksToFrame() = delete;
+
+  /// \brief Constructor for a landmarks-to-frame matching problem.
+  ///
+  /// @param[in]  frame                                       Visual frame.
+  /// @param[in]  landmarks                                   List of landmarks with descriptors.
+  /// @param[in]  image_space_distance_threshold_pixels       Max image space distance threshold for
+  ///                                                         a keypoint and a projected landmark
+  ///                                                         to become match candidates.
+  /// @param[in]  hamming_distance_threshold                  Max hamming distance for a keypoint
+  ///                                                         and a projected landmark
+  ///                                                         to become candidates.
+  MatchingProblemLandmarksToFrame(const VisualFrame& frame,
+                                  const LandmarkWithDescriptorList& landmarks,
+                                  double image_space_distance_threshold_pixels,
+                                  int hamming_distance_threshold);
+  virtual ~MatchingProblemLandmarksToFrame() {};
+
+  virtual size_t numApples() const;
+  virtual size_t numBananas() const;
+
+  /// Get a short list of candidates in list a for index b
+  ///
+  /// Return all indices of list a for n^2 matching; or use something
+  /// smarter like nabo to get nearest neighbors. Can also be used to
+  /// mask out invalid elements in the lists, or an invalid b, by
+  /// returning and empty candidate list.  
+  ///
+  /// The score for each candidate is a rough score that can be used
+  /// for sorting, pre-filtering, and will be explicitly recomputed
+  /// using the computeScore function.
+  ///
+  /// \param[in]  banana_index The index of b (landmark index) queried for candidates.
+  /// \param[out] candidates   Candidates from the frame keypoint list that could
+  ///                          potentially match the given landmark.
+  virtual void getAppleCandidatesForBanana(int landmark_index, Candidates* candidates);
+
+  inline double computeMatchScore(int hamming_distance) {
+    return static_cast<double>(384 - hamming_distance) / 384.0;
+  }
+
+  inline int computeHammingDistance(int landmark_index, int frame_keypoint_index) {
+    CHECK_LT(frame_keypoint_index, static_cast<int>(frame_descriptors_.size()))
+        << "No descriptor for this apple.";
+    CHECK_LT(landmark_index, static_cast<int>(landmark_descriptors_.size()))
+        << "No descriptor for this banana.";
+    CHECK_LT(frame_keypoint_index, static_cast<int>(valid_frame_keypoints_.size()))
+        << "No valid flag for this apple.";
+    CHECK_LT(landmark_index, static_cast<int>(valid_landmarks_.size()))
+        << "No valid flag for this apple.";
+    CHECK(valid_frame_keypoints_[frame_keypoint_index]) << "The given frame keypoint is not valid.";
+    CHECK(valid_landmarks_[landmark_index]) << "The given landmark is not valid.";
+
+    const common::FeatureDescriptorConstRef& apple_descriptor = frame_descriptors_[frame_keypoint_index];
+    const common::FeatureDescriptorConstRef& banana_descriptor = landmark_descriptors_[landmark_index];
+
+    CHECK_NOTNULL(apple_descriptor.data());
+    CHECK_NOTNULL(banana_descriptor.data());
+
+    return common::GetNumBitsDifferent(banana_descriptor, apple_descriptor);
+  }
+
+  /// \brief Gets called at the beginning of the matching problem.
+  /// Creates a y-coordinate LUT for all frame keypoints and projects all landmark into the
+  /// frame.
+  virtual bool doSetup();
+
+private:
+  /// The landmarks to be matched.
+  LandmarkWithDescriptorList landmarks_;
+  /// The visual frame the landmarks are matched against.
+  const VisualFrame& frame_;
+  /// Map mapping y coordinates in the image plane onto keypoint indices of the
+  /// visual frame keypoints.
+  std::multimap<size_t, size_t> y_coordinate_to_keypoint_index_map_;
+
+  /// Index marking frame keypoints as valid or invalid.
+  std::vector<unsigned char> valid_frame_keypoints_;
+  /// Index marking landmarks as valid or invalid.
+  std::vector<unsigned char> valid_landmarks_;
+
+  /// The landmarks projected into the visual frame, expressed in coordinate frame C.
+  aslam::Aligned<std::vector, Eigen::Vector2d>::type C_projected_landmark_keypoints_;
+
+  /// The frame keypoint descriptors.
+  std::vector<common::FeatureDescriptorConstRef> frame_descriptors_;
+
+  /// The landmark descriptors.
+  std::vector<common::FeatureDescriptorConstRef> landmark_descriptors_;
+
+  /// Descriptor size in bytes.
+  size_t descriptor_size_byes_;
+
+  /// Half width of the vertical band used for match lookup in pixels.
+  int vertical_band_halfwidth_pixels_;
+
+  /// Pairs with image space distance >= image_space_distance_threshold_pixels_ are
+  /// excluded from matches.
+  double squared_image_space_distance_threshold_pixels_squared_;
+
+  /// Pairs with descriptor distance >= hamming_distance_threshold_ are
+  /// excluded from matches.
+  int hamming_distance_threshold_;
+
+  /// The heigh of the apple frame.
+  size_t image_height_frame_;
+};
+}
+#endif //ASLAM_CV_MATCHING_PROBLEM_LANDMARKS_TO_FRAME_H_
---
+++ aslam_cv/aslam_cv_matcher/src/matching-problem-landmarks-to-frame.cc
@@ -0,0 +1,209 @@
+#include <aslam/frames/visual-frame.h>
+#include <aslam/frames/visual-nframe.h>
+#include <glog/logging.h>
+
+#include "aslam/matcher/matching-problem-landmarks-to-frame.h"
+
+namespace aslam {
+
+MatchingProblemLandmarksToFrame::MatchingProblemLandmarksToFrame(
+    const VisualFrame& frame,
+    const LandmarkWithDescriptorList& landmarks,
+    double image_space_distance_threshold_pixels,
+    int hamming_distance_threshold)
+  : landmarks_(landmarks),
+    frame_(frame),
+    squared_image_space_distance_threshold_pixels_squared_(image_space_distance_threshold_pixels *
+                                                           image_space_distance_threshold_pixels),
+    hamming_distance_threshold_(hamming_distance_threshold) {
+  CHECK_GE(hamming_distance_threshold, 0) << "Descriptor distance needs to be positive.";
+  CHECK_GE(image_space_distance_threshold_pixels, 0.0) << "Image space distance needs to be positive.";
+
+  descriptor_size_byes_ = frame.getDescriptorSizeBytes();
+
+  // The vertical search band must be at least twice the image space distance.
+  vertical_band_halfwidth_pixels_ = static_cast<int>(std::ceil(image_space_distance_threshold_pixels));
+
+  CHECK(frame.getCameraGeometry()) << "The iCam is NULL.";
+  image_height_frame_ = frame.getCameraGeometry()->imageHeight();
+  CHECK_GT(image_height_frame_, 0u) << "The visual frame has zero image rows.";
+}
+
+bool MatchingProblemLandmarksToFrame::doSetup() {
+  CHECK_GT(image_height_frame_, 0u) << "The visual frame has zero image rows.";
+
+  const size_t num_frame_keypoints = numApples();
+  const size_t num_landmarks = numBananas();
+  valid_frame_keypoints_.resize(num_frame_keypoints, false);
+  valid_landmarks_.resize(num_landmarks, false);
+
+  // First, create descriptor wrappers for all descriptors.
+  const Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic>& frame_descriptors =
+      frame_.getDescriptors();
+
+  const size_t num_frame_descriptors = static_cast<size_t>(frame_descriptors.cols());
+  CHECK_EQ(num_frame_descriptors, num_frame_keypoints) << "Mismatch between the number of "
+      << "descriptors and the number of keypoints in the visual frame.";
+
+  frame_descriptors_.clear();
+  landmark_descriptors_.clear();
+  frame_descriptors_.reserve(num_frame_descriptors);
+  landmark_descriptors_.reserve(num_landmarks);
+
+  // This creates a descriptor wrapper for the given descriptor and allows computing the hamming
+  // distance between two descriptors. todo(mbuerki): Think of ways to generalize this for
+  // different descriptor types (surf, sift, ...).
+  for (size_t frame_descriptor_idx = 0; frame_descriptor_idx < num_frame_descriptors;
+      ++frame_descriptor_idx) {
+    frame_descriptors_.emplace_back(
+        &(frame_descriptors.coeffRef(0, frame_descriptor_idx)), descriptor_size_byes_);
+  }
+
+  for (size_t landmark_descriptor_idx = 0; landmark_descriptor_idx < num_landmarks;
+      ++landmark_descriptor_idx) {
+    landmark_descriptors_.emplace_back(
+        landmarks_[landmark_descriptor_idx].getDescriptor().data(), descriptor_size_byes_);
+  }
+
+  // Then, create a LUT mapping y coordinates to frame keypoint indices.
+  const Eigen::Matrix2Xd& C_keypoints_frame = frame_.getKeypointMeasurements();
+  CHECK_EQ(static_cast<int>(num_frame_keypoints), C_keypoints_frame.cols())
+    << "The number of keypoints in the visual frame does not match the number "
+    << "of columns in the keypoint matrix.";
+
+  Camera::ConstPtr camera = frame_.getCameraGeometry();
+  CHECK(camera);
+
+  for (size_t keypoint_idx = 0; keypoint_idx < num_frame_keypoints; ++keypoint_idx) {
+    // Check if the keypoint is valid.
+    const Eigen::Vector2d& keypoint = C_keypoints_frame.col(keypoint_idx);
+    if (camera->isMasked(keypoint)) {
+      // This keypoint is masked out and hence not valid.
+      valid_frame_keypoints_[keypoint_idx] = false;
+    } else {
+      size_t y_coordinate = static_cast<size_t>(std::floor(keypoint(1)));
+      CHECK_LT(y_coordinate, image_height_frame_) << "The y coordinate for keypoint "
+          << keypoint_idx << " is bigger than or equal to the number of rows in the image.";
+
+      y_coordinate_to_keypoint_index_map_.insert(std::make_pair(y_coordinate, keypoint_idx));
+      valid_frame_keypoints_[keypoint_idx] = true;
+    }
+  }
+  VLOG(3) << "Built LUT for valid keypoints.";
+
+  C_projected_landmark_keypoints_.resize(num_landmarks);
+
+  // Then, project all landmarks into the visual frame.
+  for (size_t landmark_idx = 0u; landmark_idx < num_landmarks; ++landmark_idx) {
+
+    aslam::ProjectionResult projection_result = camera->project3(
+        landmarks_[landmark_idx].get_t_G_C(),
+        &C_projected_landmark_keypoints_[landmark_idx]);
+
+    if (projection_result.isKeypointVisible()) {
+      valid_landmarks_[landmark_idx] = true;
+    } else {
+      C_projected_landmark_keypoints_[landmark_idx].setZero();
+      valid_landmarks_[landmark_idx] = false;
+    }
+  }
+
+  VLOG(3) << "Computed all projections of landmarks into the visual frame.";
+  return true;
+}
+
+void MatchingProblemLandmarksToFrame::getAppleCandidatesForBanana(
+    int landmark_index, Candidates* candidates) {
+  // Get list of keypoint indices within some defined distance around the projected landmark
+  // keypoint and within some defined descriptor distance.
+  CHECK_EQ(numApples(), y_coordinate_to_keypoint_index_map_.size()) << "The number of apples"
+      " and the number of apples in the apple LUT differs. This can happen if 1. the visual frame "
+      "was altered between calling setup() and getAppleCandidatesForBanana(...) or 2. if the "
+      "setup() function did not build a valid LUT for the visual frame keypoints.";
+  CHECK_LT(landmark_index, static_cast<int>(valid_landmarks_.size()))
+    << "No valid flag for this landmark.";
+  CHECK_LT(landmark_index, static_cast<int>(C_projected_landmark_keypoints_.size()))
+    << "No projected keypoint for this landmark.";
+
+  CHECK_NOTNULL(candidates);
+  candidates->clear();
+  if (numApples() == 0) {
+    LOG(WARNING) << "There are zero visual frame keypoints.";
+    return;
+  }
+
+  CHECK_GT(image_height_frame_, 0u) << "The image height of the visual frame is zero.";
+
+  const Eigen::Matrix2Xd& C_keypoints_frame = frame_.getKeypointMeasurements();
+
+  if (valid_landmarks_[landmark_index]) {
+    const Eigen::Vector2d& C_keypoint_landmark = C_projected_landmark_keypoints_[landmark_index];
+
+    // Get the y coordinate of the projected landmark keypoint in the visual frame.
+    int projected_landmark_keypoint_y_coordinate = static_cast<int>(std::round(C_keypoint_landmark(1)));
+
+    // Compute the lower and upper bound of the vertical search window, making it respect the
+    // image dimension of the visual frame.
+    int y_lower_int = projected_landmark_keypoint_y_coordinate - vertical_band_halfwidth_pixels_;
+    size_t y_lower = 0;
+    if (y_lower_int > 0) y_lower = static_cast<size_t>(y_lower_int);
+    CHECK_LT(y_lower, image_height_frame_);
+
+    int y_upper_int = projected_landmark_keypoint_y_coordinate + vertical_band_halfwidth_pixels_;
+    size_t y_upper = image_height_frame_;
+    if (y_upper_int < static_cast<int>(image_height_frame_)) y_upper = y_upper_int;
+    CHECK_GE(y_upper, 0u);
+    CHECK_GT(y_upper, y_lower);
+
+    std::multimap<size_t, size_t>::iterator it_lower =
+        y_coordinate_to_keypoint_index_map_.lower_bound(y_lower);
+    if (it_lower == y_coordinate_to_keypoint_index_map_.end()) {
+      // All keypoints in the visual frame lie below the image band -> zero candidates!
+      return;
+    }
+
+    std::multimap<size_t, size_t>::iterator it_upper =
+        y_coordinate_to_keypoint_index_map_.lower_bound(y_upper);
+    if (it_upper != y_coordinate_to_keypoint_index_map_.end()) {
+      // Pointing to a valid keypoint -> need to increment this because this needs to go one
+      // beyond the border (i.e. == end() in normal for loop over a vector).
+      ++it_upper;
+    }
+
+    for (std::multimap<size_t, size_t>::iterator it = it_lower; it != it_upper; ++it) {
+      // Go over all the frame keypoints and compute image space distance to the projected landmark
+      // keypoint.
+      size_t frame_keypoint_index = it->second;
+      CHECK_LT(static_cast<int>(frame_keypoint_index), C_keypoints_frame.cols());
+      const Eigen::Vector2d& frame_keypoint = C_keypoints_frame.col(frame_keypoint_index);
+
+      double squared_image_space_distance = (frame_keypoint - C_keypoint_landmark).squaredNorm();
+
+      if (squared_image_space_distance < squared_image_space_distance_threshold_pixels_squared_) {
+        // This one is within the radius. Compute the descriptor distance.
+        int hamming_distance = computeHammingDistance(landmark_index, frame_keypoint_index);
+
+        if (hamming_distance < hamming_distance_threshold_) {
+          CHECK_GE(hamming_distance, 0);
+          int priority = 0;
+          candidates->emplace_back(frame_keypoint_index,
+                                   landmark_index,
+                                   computeMatchScore(hamming_distance),
+                                   priority);
+        }
+      }
+    }
+  } else {
+    LOG(WARNING) << "Landmark " << landmark_index << " is not valid.";
+  }
+}
+
+size_t MatchingProblemLandmarksToFrame::numApples() const {
+  return static_cast<size_t>(frame_.getNumKeypointMeasurements());
+}
+
+size_t MatchingProblemLandmarksToFrame::numBananas() const {
+  return landmarks_.size();
+}
+
+}  // namespace aslam
