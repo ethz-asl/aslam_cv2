ca1b66cc9b40243f39de4dc06b18cd344c911309
Titus Cieslewski <dev@titus-c.ch>
2015-12-20 00:20:09 +0100

    WIP

--- aslam_cv/aslam_cv_frames/src/visual-frame.cc
+++ aslam_cv/aslam_cv_frames/src/visual-frame.cc
@@ -1,7 +1,9 @@
 #include <memory>
-#include <aslam/frames/visual-frame.h>
 #include <aslam/common/channel-definitions.h>
+#include <aslam/common/stl-helpers.h>
 #include <aslam/common/time.h>
+
+#include "aslam/frames/visual-frame.h"
 
 namespace aslam {
 VisualFrame::VisualFrame()
@@ -451,4 +453,62 @@ VisualFrame::Ptr VisualFrame::createEmptyTestVisualFrame(const aslam::Camera::Co
   frame->setId(id);
   return frame;
 }
+
+void VisualFrame::discardUntrackedObservations() {
+  CHECK(hasTrackIds());
+  LOG(INFO) << 1;
+  const Eigen::VectorXi& track_ids = getTrackIds();
+  LOG(INFO) << 1;
+  const int original_count = track_ids.rows();
+  LOG(INFO) << 1;
+  std::vector<size_t> untracked_indices;
+  for (int i = 0; i < original_count; ++i) {
+    if (track_ids(i) == -1) {
+      untracked_indices.emplace_back(i);
+    }
+  }
+  LOG(INFO) << 1;
+  if (untracked_indices.empty()) {
+    return;
+  }
+  LOG(INFO) << 1;
+
+  if (hasKeypointMeasurements()) {
+    common::stl_helpers::eraseIndicesFromContainer(
+        untracked_indices, original_count, getKeypointMeasurementsMutable());
+  }
+  LOG(INFO) << 1;
+  if (hasKeypointMeasurementUncertainties()) {
+    common::stl_helpers::eraseIndicesFromContainer(
+        untracked_indices, original_count,
+        getKeypointMeasurementUncertaintiesMutable());
+  }
+  LOG(INFO) << 1;
+  if (hasKeypointOrientations()) {
+    common::stl_helpers::eraseIndicesFromContainer(
+        untracked_indices, original_count, getKeypointOrientationsMutable());
+  }
+  LOG(INFO) << 1;
+  if (hasKeypointScores()) {
+    common::stl_helpers::eraseIndicesFromContainer(
+        untracked_indices, original_count, getKeypointScoresMutable());
+  }
+  LOG(INFO) << 1;
+  if (hasKeypointScales()) {
+    common::stl_helpers::eraseIndicesFromContainer(
+        untracked_indices, original_count, getKeypointScalesMutable());
+  }
+  LOG(INFO) << 1;
+  if (hasDescriptors()) {
+    common::stl_helpers::OneDimensionAdapter<unsigned char,
+    common::stl_helpers::kColumns> adapter(getDescriptorsMutable());
+    common::stl_helpers::eraseIndicesFromContainer(
+        untracked_indices, original_count, &adapter);
+  }
+  LOG(INFO) << 1;
+  common::stl_helpers::eraseIndicesFromContainer(
+      untracked_indices, original_count, getTrackIdsMutable());
+  LOG(INFO) << 1;
+}
+
 }  // namespace aslam
