2f0f601973204e95ee5b42fe4640db0b8fad305d
Mathias Buerki <mathias.buerki@mavt.ethz.ch>
2016-05-14 17:24:50 +0200

    added missing file

---
+++ aslam_cv/aslam_cv_visualization/include/aslam/visualization/basic-visualization-inl.h
@@ -0,0 +1,33 @@
+#ifndef SLAM_VISUALIZATION_BASIC_VISUALIZATION_INL_H_
+#define SLAM_VISUALIZATION_BASIC_VISUALIZATION_INL_H_
+
+#include <aslam/matcher/match.h>
+#include <aslam/matcher/match-helpers.h>
+
+namespace aslam_cv_visualization {
+
+template<typename MatchesWithScore>
+void visualizeMatches(const aslam::VisualFrame& frame_kp1,
+                      const aslam::VisualFrame& frame_k,
+                      const MatchesWithScore& matches_with_score,
+                      cv::Mat* image) {
+  CHECK_NOTNULL(image);
+
+  cv::Mat& match_image = *image;
+  cv::cvtColor(frame_kp1.getRawImage(), match_image, CV_GRAY2BGR);
+
+  VLOG(4) << "Converted raw image from grayscale to color.";
+
+  aslam::Matches matches;
+  aslam::convertMatches<MatchesWithScore>(matches_with_score, &matches);
+  VLOG(4) << "Converted the matches.";
+
+  CHECK_NOTNULL(match_image.data);
+
+  drawKeypointMatches(frame_kp1, frame_k, matches, cv::Scalar(255, 255, 0), cv::Scalar(255, 0, 255),
+                      &match_image);
+}
+
+}  // namespace aslam_cv_visualization
+
+#endif // SLAM_VISUALIZATION_BASIC_VISUALIZATION_INL_H_
