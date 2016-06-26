164cb506314e86b7876fa4d5341cd5251eabf487
Mathias Gehrig <mathias.gehrig@gmail.com>
2016-06-27 00:13:41 +0200

    combined rotation + translation ransac

--- aslam_cv/aslam_cv_geometric_vision/include/aslam/geometric-vision/match-outlier-rejection-twopt.h
+++ aslam_cv/aslam_cv_geometric_vision/include/aslam/geometric-vision/match-outlier-rejection-twopt.h
@@ -12,9 +12,9 @@ class VisualFrame;
 
 namespace geometric_vision {
 
-/// \brief Runs a 2-pt RANSAC scheme over the specified match list to separate matches into
-///        out-/inliers. The bearing vectors are unrotated using a given rotation q_Ckp1_Ck
-///        and a model is used that only estimates the translational motion.
+/// \brief Runs two RANSAC schemes over the specified match list to separate matches into
+///        out-/inliers. Both a translation only and rotation only RANSAC are computed.
+///        The final set of inliers is the union of both inlier sets.
 /// @param[in]  frame_kp1  Current frame.
 /// @param[in]  frame_k    Previous frame.
 /// @param[in]  q_Ckp1_Ck  Rotation taking points from the camera frame k to the camera frame k+1.
@@ -26,34 +26,15 @@ namespace geometric_vision {
 /// @param[out] inlier_matches_kp1_k The list of inlier matches.
 /// @param[out] outlier_matches_kp1_k The list of outlier matches.
 /// @return RANSAC successful?
-bool rejectOutlierFeatureMatchesTranslationSAC(
+bool rejectOutlierFeatureMatchesTranslationRotationSAC(
     const aslam::VisualFrame& frame_kp1, const aslam::VisualFrame& frame_k,
-    const aslam::Quaternion& q_Ckp1_Ck, const aslam::MatchesWithScore& matches_kp1_k,
-    bool fix_random_seed, double ransac_threshold,
-    size_t ransac_max_iterations, aslam::MatchesWithScore* inlier_matches_kp1_k,
-    aslam::MatchesWithScore* outlier_matches_kp1_k);
-
-/// \brief Runs two RANSAC schemes over the specified match list to separate matches into
-///        out-/inliers. Both a rotation only and relative pose ransac are computed. The model
-///        with the highest number of inliers is chosen.
-/// @param[in]  frame_kp1  Current frame.
-/// @param[in]  frame_k    Previous frame.
-/// @param[in]  matches_kp1_k The matches between the frames.
-/// @param[in]  fix_random_seed Use a fixed random seed for RANSAC.
-/// @param[in]  ransac_threshold  RANSAC threshold to consider a sample as inlier.
-///                               The threshold is defined as:  1 - cos(max_ray_disparity_angle).
-/// @param[in]  ransac_max_iterations Max. RANSAC iterations.
-/// @param[out] inlier_matches_kp1_k The list of inlier matches.
-/// @param[out] outlier_matches_kp1_k The list of outlier matches.
-/// @return RANSAC successful?
-bool rejectOutlierFeatureMatchesRelativePoseRotationSAC(
-    const aslam::VisualFrame& frame_kp1, const aslam::VisualFrame& frame_k,
+    const aslam::Quaternion& q_Ckp1_Ck,
     const aslam::MatchesWithScore& matches_kp1_k, bool fix_random_seed,
     double ransac_threshold, size_t ransac_max_iterations,
     aslam::MatchesWithScore* inlier_matches_kp1_k,
     aslam::MatchesWithScore* outlier_matches_kp1_k);
 
-}  // namespace gv
+}  // namespace geometric_vision
 
 }  // namespace aslam
 #endif  // ASLAM_MATCH_OUTLIER_REJECTION_TWOPT_H_
