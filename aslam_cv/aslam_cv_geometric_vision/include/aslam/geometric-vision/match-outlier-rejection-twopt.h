0f6ec955a10d14a072b64c86e191d8a05bba2d7b
Mathias Gehrig <mathias.gehrig@gmail.com>
2016-06-26 22:36:24 +0200

    add description of outlier rejecter

--- aslam_cv/aslam_cv_geometric_vision/include/aslam/geometric-vision/match-outlier-rejection-twopt.h
+++ aslam_cv/aslam_cv_geometric_vision/include/aslam/geometric-vision/match-outlier-rejection-twopt.h
@@ -33,11 +33,24 @@ bool rejectOutlierFeatureMatchesTranslationSAC(
     size_t ransac_max_iterations, aslam::MatchesWithScore* inlier_matches_kp1_k,
     aslam::MatchesWithScore* outlier_matches_kp1_k);
 
+/// \brief Runs two RANSAC schemes over the specified match list to separate matches into
+///        out-/inliers. Both a rotation only and relative pose ransac are computed. The model
+///        with the highest number of inliers is chosen.
+/// @param[in]  frame_kp1  Current frame.
+/// @param[in]  frame_k    Previous frame.
+/// @param[in]  matches_kp1_k The matches between the frames.
+/// @param[in]  fix_random_seed Use a fixed random seed for RANSAC.
+/// @param[in]  ransac_threshold  RANSAC threshold to consider a sample as inlier.
+///                               The threshold is defined as:  1 - cos(max_ray_disparity_angle).
+/// @param[in]  ransac_max_iterations Max. RANSAC iterations.
+/// @param[out] inlier_matches_kp1_k The list of inlier matches.
+/// @param[out] outlier_matches_kp1_k The list of outlier matches.
+/// @return RANSAC successful?
 bool rejectOutlierFeatureMatchesRelativePoseRotationSAC(
     const aslam::VisualFrame& frame_kp1, const aslam::VisualFrame& frame_k,
-    const aslam::Quaternion& q_Ckp1_Ck, const aslam::MatchesWithScore& matches_kp1_k,
-    bool fix_random_seed, double ransac_threshold,
-    size_t ransac_max_iterations, aslam::MatchesWithScore* inlier_matches_kp1_k,
+    const aslam::MatchesWithScore& matches_kp1_k, bool fix_random_seed,
+    double ransac_threshold, size_t ransac_max_iterations,
+    aslam::MatchesWithScore* inlier_matches_kp1_k,
     aslam::MatchesWithScore* outlier_matches_kp1_k);
 
 }  // namespace gv
