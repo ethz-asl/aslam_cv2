02da766a89dc3e4a580cb2e5f99c9e3d2e641a1e
magehrig <mathias.gehrig@gmail.com>
2016-11-16 15:06:39 +0100

    fix undistorter test
    Use cv::convertMaps function of OpenCV 2.4.13.1 branch

---
+++ aslam_cv/aslam_cv_pipeline/include/aslam/pipeline/test/convert-maps-legacy.h
@@ -0,0 +1,15 @@
+#ifndef ASLAM_TEST_CONVERT_MAPS_LEGACY_H
+#define ASLAM_TEST_CONVERT_MAPS_LEGACY_H
+
+#include <opencv2/imgproc/imgproc.hpp>
+
+namespace aslam {
+
+// This function is a copy of the convertMaps function of OpenCV's 2.4.13.1
+// branch to circumvent a SSE2 bug that was introduced with OpenCV 3.*.
+void convertMapsLegacy(cv::InputArray _map1, cv::InputArray _map2,
+                       cv::OutputArray _dstmap1, cv::OutputArray _dstmap2,
+                       int dstm1type, bool nninterpolate);
+} // namespace aslam
+
+#endif // ASLAM_TEST_CONVERT_MAPS_LEGACY_H
