f1f2874454b0ae7c81c9701e285f16510a2e68c5
Fabian Blöchliger <f.bloechliger@gmail.com>
2018-01-08 22:20:58 +0100

    Adapted detect_simd.cmake script for OSX.

--- aslam_cv_common/cmake/detect_simd.cmake
+++ aslam_cv_common/cmake/detect_simd.cmake
@@ -1,25 +1,20 @@
 # Detect the preprocessor directives which are set by the compiler.
-execute_process(COMMAND ${CMAKE_CXX_COMPILER} -dM -E -x c /dev/null
+execute_process(COMMAND ${CMAKE_CXX_COMPILER} -march=native -dM -E -x c /dev/null
                 OUTPUT_VARIABLE PREPROCESSOR_DIRECTIVES)
-execute_process(COMMAND cat /proc/cpuinfo COMMAND grep flags
-                OUTPUT_VARIABLE CPU_FLAGS)
 
 set(IS_SSE_ENABLED FALSE)
 set(IS_NEON_ENABLED FALSE)
-if (PREPROCESSOR_DIRECTIVES MATCHES "__SSE2__")
-  if(${CPU_FLAGS} MATCHES "sse3")
-    add_definitions(-mssse3)
-    set(IS_SSE_ENABLED TRUE)
-  else()
-    message(FATAL_ERROR "SSE3 instruction set not available.")
-  endif()
+if (PREPROCESSOR_DIRECTIVES MATCHES "__SSSE3__")
+  add_definitions(-mssse3)
+  set(IS_SSE_ENABLED TRUE)
 # For both armv7 and armv8, __ARM_NEON is used as preprocessor directive.
 elseif (PREPROCESSOR_DIRECTIVES MATCHES "__ARM_ARCH 7")
   add_definitions(-mfpu=neon) # Needs to be set for armv7.
   set(IS_NEON_ENABLED TRUE)
 elseif (PREPROCESSOR_DIRECTIVES MATCHES "__ARM_ARCH 8")
   set(IS_NEON_ENABLED TRUE)
+else()
+  message(WARNING "No SIMD instruction set detected.")
 endif()
 
 unset(PREPROCESSOR_DIRECTIVES CACHE)
-unset(CPU_FLAGS CACHE)
