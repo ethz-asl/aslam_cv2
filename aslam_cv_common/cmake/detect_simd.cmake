cf35821e7d37a3f6e33c5635e55212061ae637fe
Fabian Blöchliger <f.bloechliger@gmail.com>
2018-01-08 21:44:25 +0100

    Updated detect_simd script.

--- aslam_cv_common/cmake/detect_simd.cmake
+++ aslam_cv_common/cmake/detect_simd.cmake
@@ -1,12 +1,19 @@
 # Detect the preprocessor directives which are set by the compiler.
 execute_process(COMMAND ${CMAKE_CXX_COMPILER} -dM -E -x c /dev/null
                 OUTPUT_VARIABLE PREPROCESSOR_DIRECTIVES)
+execute_process(COMMAND cat /proc/cpuinfo COMMAND grep flags
+                OUTPUT_VARIABLE CPU_FLAGS)
 
 set(IS_SSE_ENABLED FALSE)
 set(IS_NEON_ENABLED FALSE)
 if (PREPROCESSOR_DIRECTIVES MATCHES "__SSE2__")
-  add_definitions(-mssse3)
-  set(IS_SSE_ENABLED TRUE)
+  if(${CPU_FLAGS} MATCHES "sse3")
+    add_definitions(-mssse3)
+    set(IS_SSE_ENABLED TRUE)
+    message(WARNING "asdf")
+  else()
+    message(FATAL_ERROR "SSE3 instruction set not available.")
+  endif()
 # For both armv7 and armv8, __ARM_NEON is used as preprocessor directive.
 elseif (PREPROCESSOR_DIRECTIVES MATCHES "__ARM_ARCH 7")
   add_definitions(-mfpu=neon) # Needs to be set for armv7.
@@ -14,3 +21,6 @@ elseif (PREPROCESSOR_DIRECTIVES MATCHES "__ARM_ARCH 7")
 elseif (PREPROCESSOR_DIRECTIVES MATCHES "__ARM_ARCH 8")
   set(IS_NEON_ENABLED TRUE)
 endif()
+
+unset(PREPROCESSOR_DIRECTIVES CACHE)
+unset(CPU_FLAGS CACHE)
