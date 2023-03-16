f9333dbcbb73a29afdaef6298e2cf3a16e85e3d3
BenjaminKrummenacher <benjamin.krummenacher@outlook.com>
2023-03-17 00:04:34 +0100

    remove old unused function

--- aslam_cv_cameras/include/aslam/cameras/camera-generic-inl.h
+++ aslam_cv_cameras/include/aslam/cameras/camera-generic-inl.h
@@ -5,45 +5,6 @@
 
 namespace aslam {
 
-// TODO(dymczykm) actually, I'm not sure if it wouldn't be better if
-// specialized versions of these function (for double) would use this
-// implementation instead of repeating it twice. But that would mean we need
-// to template the whole class on ScalarType (or at least intrinsics and few
-// methods).
-
-template <typename ScalarType, typename DistortionType,
-          typename MIntrinsics, typename MDistortion>
-const ProjectionResult GenericCamera::project3Functional(
-    const Eigen::Matrix<ScalarType, 3, 1>& point_3d,
-    const Eigen::MatrixBase<MIntrinsics>& intrinsics_external,
-    const Eigen::MatrixBase<MDistortion>& distortion_coefficients_external,
-    Eigen::Matrix<ScalarType, 2, 1>* out_keypoint) const {
-
-  CHECK_NOTNULL(out_keypoint);
-  CHECK_EQ(intrinsics_external.size(), kNumOfParams) << "intrinsics: invalid size!";
-
-  const ScalarType& fu = intrinsics_external[0];
-  const ScalarType& fv = intrinsics_external[1];
-  const ScalarType& cu = intrinsics_external[2];
-  const ScalarType& cv = intrinsics_external[3];
-
-  ScalarType rz = static_cast<ScalarType>(1.0) / point_3d[2];
-  Eigen::Matrix<ScalarType, 2, 1> keypoint;
-  keypoint[0] = point_3d[0] * rz;
-  keypoint[1] = point_3d[1] * rz;
-
-  // Distort the point (if a distortion model is set)
-  const DistortionType& distortion =
-      static_cast<const DistortionType&>(*distortion_);
-  distortion.distortUsingExternalCoefficients(
-      distortion_coefficients_external, keypoint, &keypoint);
-
-  (*out_keypoint)[0] = fu * keypoint[0] + cu;
-  (*out_keypoint)[1] = fv * keypoint[1] + cv;
-
-  return evaluateProjectionResult(*out_keypoint, point_3d);
-}
-
 template <typename DerivedKeyPoint, typename DerivedPoint3d>
 inline const ProjectionResult GenericCamera::evaluateProjectionResult(
     const Eigen::MatrixBase<DerivedKeyPoint>& keypoint,
